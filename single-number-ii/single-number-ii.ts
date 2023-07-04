75c785ba0b206a4e8fa47042f77ee7d4bf739512
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-07-04 09:52:12 +0700

    Time: 72 ms (45.33%) | Memory: 45.4 MB (41.33%) - LeetSync

--- single-number-ii/single-number-ii.ts
+++ single-number-ii/single-number-ii.ts
@@ -1,4 +1,8 @@
 function singleNumber(nums: number[]): number {
+    return usingMath(nums)
+};
+
+function usingMath(nums: number[]): number {
     const seen = new Set();
     let totalDistinct = 0;
     let total = 0;
@@ -10,4 +14,18 @@ function singleNumber(nums: number[]): number {
         } 
     }
     return (3 * totalDistinct - total) / 2
-};
+}
+
+function usingHashmap(nums: number[]): number {
+    const candidate = new Set();
+    const seen = new Set();
+    for(const n of nums) {
+        if(seen.has(n)) {
+            candidate.delete(n)
+        } else {
+            seen.add(n)
+            candidate.add(n)
+        }
+    }
+    return candidate.values().next().value
+}

62280105c2a064b078c5e33569f8644cef1732fa
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-07-05 09:56:18 +0700

    Time: 63 ms (66.67%) | Memory: 44.6 MB (33.33%) - LeetSync

--- find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.ts
+++ find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.ts
@@ -1,55 +1,15 @@
 function tictactoe(moves: number[][]): string {
-    let winner = -1;
-    const boards = new Array(3).fill(0).map(()=>new Array(3).fill(0))
-    for(let i = 0; i < moves.length; i++) {
-        const [x, y] = moves[i]
-        if(i % 2) {
-            boards[x][y] = 2
-        } else {
-            boards[x][y] = 1
-        }
+    let turn = 1;
+    const boards = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]]
+    for(const [row, col] of moves) {
+        const player = turn % 2;
+        boards[row][col] = player;
+        const validRow = boards[row][0] === boards[row][1] && boards[row][1] === boards[row][2]  
+        const validCol = boards[0][col] === boards[1][col] && boards[1][col] === boards[2][col]  
+        const validD1 = boards[0][0] !== -1 && boards[0][0] === boards[1][1] && boards[1][1] === boards[2][2]
+        const validD2 = boards[0][2] !== -1 && boards[0][2] === boards[1][1] && boards[1][1] === boards[2][0]
+        if(validRow || validCol || validD1 || validD2) return player === 1 ? 'A': 'B'
+        turn++;
     }
-    let counter = 0;
-    // horizontal
-    for(let i = 0; i < 3; i++) {
-        if(!boards[i][0]) continue;
-        counter = 1
-        for(let k = 1; k < 3; k++) {
-            if(boards[i][0] !== boards[i][k]) break;
-            counter++
-        }
-        if(counter === 3) return boards[i][0] === 1 ? 'A' : 'B'
-    }
-    // vertical
-    for(let i = 0; i < 3; i++) {
-        if(!boards[0][i]) continue;
-        counter = 1
-        for(let k = 1; k < 3; k++) {
-            if(boards[0][i] !== boards[k][i]) break;
-            counter++
-        }
-        if(counter === 3) return boards[0][i] === 1 ? 'A' : 'B'
-    }
-    // diagonal top left to bottom right
-    if(boards[0][0]) {
-        counter = 1;
-        for(let i = 1; i < 3; i++) {
-            if(boards[0][0] !== boards[i][i]) break;
-            counter++
-        }
-        console.log(boards[0][0], counter)
-        if(counter === 3) return boards[0][0] === 1 ? 'A' : 'B'
-    }
-    // diagonal top right to bottom left
-    if(boards[0][2]) {
-        counter = 1;
-        for(let i = 1; i < 3; i++) {
-            if(boards[0][2] !== boards[0 + i][2 - i]) break;
-            counter++
-        }
-        console.log(boards[0][2], counter)
-        if(counter === 3) return boards[0][2] === 1 ? 'A' : 'B'
-    }
-    if(moves.length < 9) return 'Pending'
-    return 'Draw';
+    return moves.length < 9 ? 'Pending' : 'Draw'
 };

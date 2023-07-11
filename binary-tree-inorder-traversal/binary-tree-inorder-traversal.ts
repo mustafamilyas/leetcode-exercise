8c34aa3ed4345400b5f0a70e1fd1f6020f723992
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-07-11 19:45:17 +0700

    Time: 61 ms (72.93%) | Memory: 44.6 MB (39.49%) - LeetSync

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -16,3 +16,41 @@ function inorderTraversal(root: TreeNode | null): number[] {
     if(root === null) return []
     return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
 };
+
+function iterativeInorderTraversal (root: TreeNode | null): number[] {
+    const stack = [];
+    const res = [];
+    let currentNode = root;
+    
+    while(stack.length || currentNode !== null) {
+        if (currentNode) {
+            stack.push(currentNode);
+            currentNode = currentNode.left
+        } else {
+            const prevNode = stack.pop();
+            res.push(prevNode.val);
+            currentNode = prevNode.right;
+        }
+    }
+    
+    return res;
+};
+
+function iterativeInorderTraversal2 (root: TreeNode | null): number[] {
+    const stack = [];
+    const res = [];
+    let currentNode = root;
+    
+    while(stack.length || currentNode) {
+        while(currentNode) {
+            stack.push(currentNode);
+            currentNode = currentNode.left;
+        }
+        
+        currentNode = stack.pop();
+        res.push(currentNode.val);
+        currentNode = currentNode.right;
+    }
+    
+    return res;
+};

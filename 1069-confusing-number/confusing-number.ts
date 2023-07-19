385a4abbf4483e71a1be6b57dd3db583b5265e5e
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-07-19 20:04:07 +0700

    Time: 64 ms (40.91%) | Memory: 42 MB (100.00%) - LeetSync

---
+++ 1069-confusing-number/confusing-number.ts
@@ -0,0 +1,15 @@
+function confusingNumber(n: number): boolean {
+    let temp = n;
+    const rotateNumber = [0, 1, null, null, null, null, 9, null, 8, 6];
+    let value = 0;    
+    while(temp > 9) {
+        const remainder = temp % 10;
+        if(rotateNumber[remainder] === null) return false; 
+        value = value * 10 + rotateNumber[remainder];
+        temp = Math.floor(temp / 10);
+    }
+    const nextNumber = rotateNumber[temp];
+    if(nextNumber === null) return false;
+    value = value * 10 + nextNumber;
+    return n !== value;
+};

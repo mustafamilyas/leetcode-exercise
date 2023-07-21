e55a34d0ee13081659e96bf3cca4e76288941b10
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-07-21 22:07:17 +0700

    Time: 76 ms (16.57%) | Memory: 43.2 MB (27.36%) - LeetSync

--- 2749-promise-time-limit/promise-time-limit.ts
+++ 2749-promise-time-limit/promise-time-limit.ts
@@ -2,12 +2,11 @@ type Fn = (...params: any[]) => Promise<any>;
 
 function timeLimit(fn: Fn, t: number): Fn {
 	return async function(...args) {
-        return new Promise((resolve, reject)=>{
-            fn(...args).then(resolve).catch(err=>reject(err))
-            setTimeout(()=>{
-                reject("Time Limit Exceeded")
-            }, t)
+        const rejectPromise = new Promise((_, reject)=> {
+            setTimeout(()=>reject('Time Limit Exceeded'), t)
         })
+        const fnPromise = fn(...args)
+        return Promise.race([rejectPromise, fnPromise])
     }
 };
 

0209ee5e15be335281fb5febb83c3b69b3225cc7
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-08-02 08:09:07 +0700

    Time: 67 ms (86.27%) | Memory: 45.3 MB (83.61%) - LeetSync

--- 46-permutations/permutations.ts
+++ 46-permutations/permutations.ts
@@ -1,18 +1,18 @@
 function permute(nums: number[]): number[][] {
     const result = []
-    function recurse(combine: number[]) {
-        if(combine.length === nums.length) {
-            result.push([...combine])
+    function recurse(combine: Set<number>) {
+        if(combine.size === nums.length) {
+            result.push(Array.from(combine))
             return;
         }
         for(const n of nums) {
-            if(combine.findIndex((i)=>i === n) === -1) {
-                combine.push(n)
+            if(!combine.has(n)) {
+                combine.add(n)
                 recurse(combine)
-                combine.pop()
+                combine.delete(n)
             }
         }
     }
-    recurse([])
+    recurse(new Set())
     return result;
 };

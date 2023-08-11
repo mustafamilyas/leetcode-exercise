103b56f4b1ced166464dae75a148880953e74bb0
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-08-11 11:14:59 +0700

    Time: 63 ms (92.23%) | Memory: 44.4 MB (83.50%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.ts
+++ 518-coin-change-ii/coin-change-ii.ts
@@ -1,6 +1,8 @@
 function change(amount: number, coins: number[]): number {
     const memo = new Array(amount + 1).fill(0);
+    // the purpose of this, is to increment value, where remaining equal to zero
     memo[0] = 1
+    
     for(const coin of coins) {
         for(let target = coin; target <= amount; target++) {
             memo[target] += (memo[target - coin] ?? 0)

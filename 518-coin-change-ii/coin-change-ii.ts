0e539c884b885e711719a82c68f3aa5b7b775465
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-08-11 11:13:50 +0700

    Time: 66 ms (88.35%) | Memory: 43.6 MB (100.00%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.ts
+++ 518-coin-change-ii/coin-change-ii.ts
@@ -1,30 +1,11 @@
 function change(amount: number, coins: number[]): number {
-    const cols = amount + 1; 
-    const rows = coins.length + 1;  
-    const cache: number[][] = [];
-    
-    for (let row = 0; row < rows; row++) {
-        cache.push(new Array(cols).fill(0));
-        cache[row][0] = 1;
-    }
-    
-    for (let col = 1; col < cols; col++) {
-        for (let row = rows - 2; row >= 0; row--) {
-            const target = col;
-            const remainder = target - coins[row];
-            
-            if (remainder > 0) {
-                cache[row][col] += cache[row][remainder];
-                cache[row][col] += cache[row + 1][col];              
-            } else if (remainder < 0) {
-                cache[row][col] += cache[row + 1][col];                
-            } else if (remainder == 0) {
-                cache[row][col] += 1; 
-                cache[row][col] += cache[row + 1][col];
-            }
-            
+    const memo = new Array(amount + 1).fill(0);
+    memo[0] = 1
+    for(const coin of coins) {
+        for(let target = coin; target <= amount; target++) {
+            memo[target] += (memo[target - coin] ?? 0)
         }
     }
-    
-    return cache[0][cols - 1];
+
+    return memo[amount];
 };

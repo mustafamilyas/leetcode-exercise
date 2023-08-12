d7dfc8066b7d4212d815f721b1fcba4e76bb8041
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-08-12 12:14:54 +0700

    Time: 68 ms (53.13%) | Memory: 43.3 MB (90.63%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.ts
+++ 63-unique-paths-ii/unique-paths-ii.ts
@@ -1,4 +1,6 @@
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
+    if(obstacleGrid[0][0]) return 0;
+    
     const m = obstacleGrid.length;
     const n = obstacleGrid[0].length;
     const dp = new Array(m).fill(null).map(()=>new Array(n).fill(0))
@@ -6,10 +8,8 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
 
     for(let i = 0; i < m; i++) {
         for(let j = 0; j < n; j++) {
-            if(i === 0 && j === 0) {
-                if(obstacleGrid[i][j]) return 0;
-                continue;
-            }
+            if(i === 0 && j === 0) continue;
+            
             if(obstacleGrid[i][j]) {
                 dp[i][j] = 0;
             } else {

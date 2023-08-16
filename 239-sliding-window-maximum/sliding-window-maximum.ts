b2566b7e1a547dccb9d6d8ffe7f8f6e94d227552
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-08-16 20:58:43 +0700

    Time: 682 ms (59.47%) | Memory: 73.1 MB (51.58%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.ts
+++ 239-sliding-window-maximum/sliding-window-maximum.ts
@@ -1,23 +1,19 @@
 function maxSlidingWindow(nums: number[], k: number): number[] {
     const result: number[] = [];
-    const deque: number[] = []; // Store indices, not actual elements
+    const deque: number[] = [];
     
-    for (let i = 0; i < nums.length; i++) {
-        // Remove elements that are out of the current window
-        while (deque.length > 0 && deque[0] < i - k + 1) {
+    for(let i = 0; i < nums.length; i++) {
+        while(deque.length && deque[0] <= i - k) {
+            // shift the sliding window
             deque.shift();
         }
-        
-        // Remove elements that are smaller than the current element
-        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+        while(deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+            // pop if the top is less than cur number
             deque.pop();
         }
-        
-        deque.push(i); // Add the current element's index to the deque
-        
-        // If the left end of the window is inside the array, add maximum to the result
-        if (i >= k - 1) {
-            result.push(nums[deque[0]]);
+        deque.push(i)
+        if(i >= k - 1) {
+            result.push(nums[deque[0]])
         }
     }
     

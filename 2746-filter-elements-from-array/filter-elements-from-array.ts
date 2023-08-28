b9581f8345440a684eb8407685cf674a5303304a
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-08-28 07:59:43 +0700

    Time: 53 ms (82.70%) | Memory: 42.6 MB (81.78%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.ts
@@ -0,0 +1,8 @@
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    const result = [];
+    for(let i = 0; i < arr.length; i++) {
+        const isValid = fn(arr[i], i);
+        if(isValid) result.push(arr[i])
+    }
+    return result;
+};

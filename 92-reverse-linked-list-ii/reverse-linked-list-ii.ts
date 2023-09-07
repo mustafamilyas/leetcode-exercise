41322bf8b55dfdc8b1fef6330ee9a0f6bedb9832
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-09-07 08:01:42 +0700

    Time: 61 ms (42.79%) | Memory: 43.9 MB (85.58%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.ts
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.ts
@@ -12,15 +12,10 @@
 
 function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
     if(left === right) return head;
-
     let leftNode = null, prev = null, cur = head;
     let counter = 1;
-
     while(cur && counter <= right) {
-        if(counter < left) {
-            leftNode = cur;
-        }
-
+        if(counter < left) leftNode = cur;
         if(counter <= left) {
             prev = cur;
             cur = cur.next;
@@ -36,9 +31,7 @@ function reverseBetween(head: ListNode | null, left: number, right: number): Lis
     if(leftNode) {
         const endFlipped = leftNode.next;
         leftNode.next = prev;
-        if(endFlipped) {
-            endFlipped.next = cur;
-        }
+        if(endFlipped) endFlipped.next = cur;
         return head;
     } else {
         head.next = cur;

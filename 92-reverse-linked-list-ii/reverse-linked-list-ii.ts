b843b7c493690b468fbf03ff1e67b6b285464871
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-09-07 08:00:54 +0700

    Time: 53 ms (80.77%) | Memory: 44.4 MB (46.15%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.ts
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
+    if(left === right) return head;
+
+    let leftNode = null, prev = null, cur = head;
+    let counter = 1;
+
+    while(cur && counter <= right) {
+        if(counter < left) {
+            leftNode = cur;
+        }
+
+        if(counter <= left) {
+            prev = cur;
+            cur = cur.next;
+        } else {
+            const next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        counter++
+    }
+
+    if(leftNode) {
+        const endFlipped = leftNode.next;
+        leftNode.next = prev;
+        if(endFlipped) {
+            endFlipped.next = cur;
+        }
+        return head;
+    } else {
+        head.next = cur;
+        return prev;
+    }
+};

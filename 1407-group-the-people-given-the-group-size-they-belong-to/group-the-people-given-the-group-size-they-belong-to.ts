d85661c1b1a26e65605eca4e35ae45f943bc7a82
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-09-11 10:27:47 +0700

    Time: 78 ms (87.10%) | Memory: 48.3 MB (77.42%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.ts
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.ts
@@ -1,18 +1,15 @@
 function groupThePeople(groupSizes: number[]): number[][] {
     const map: Record<number, number[]> = {};
+    const result = [];
     for(let i = 0; i < groupSizes.length; i++) {
         const g = groupSizes[i]
         if(map[g]) map[g].push(i)
         else map[g] = [i];
-    }
 
-    const result = [];
-    Object.entries(map).forEach(([key, value])=> {
-        const keyInt = parseInt(key)
-        for(let i = 0; i < value.length; i += keyInt) {
-            result.push(value.slice(i, i + keyInt))
+        if(map[g].length === g) {
+            result.push(map[g])
+            map[g] = []
         }
-
-    })
+    }
     return result;
 };

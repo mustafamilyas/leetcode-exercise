e5ddcd3ed4efa03e7be631c700f74c6a0ddbb905
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-09-14 05:52:48 +0700

    Time: 89 ms (45.56%) | Memory: 45.1 MB (90.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,16 +1,26 @@
 function lengthOfLongestSubstring(s: string): number {
-    let left = 0, right = 0, res = 0;
-    const map = {};
-    while(left < s.length && right < s.length) {
-        map[s[right]] = (map?.[s[right]] ?? 0) + 1;
-        if(map[s[right]] === 2) {
-            res = Math.max(res, right - left);
-            for(; left < right && map[s[right]] === 2; left++) {
-                map[s[left]]--
+    const charMap = new Array(26).fill(-1);
+    let start = 0, end = 0, max = 0;
+    
+    for(; end < s.length; end++) {
+        const currentIndex = s.charCodeAt(end) - 97;
+        if(charMap[currentIndex] > -1) {
+            // calculate max
+            max = Math.max(end - start, max);
+            // slide window
+            while(start < end) {
+                charMap[s.charCodeAt(start) - 97] = -1;
+                if(start !== end && s.charCodeAt(end) === s.charCodeAt(start)) {
+                    start++;    
+                    break;
+                }
+                start++;
             }
+            charMap[currentIndex] = end;
+        } else {
+            charMap[currentIndex] = end;
         }
-        right++;
     }
-    res = Math.max(res, right - left);
-    return res;
+    if(start !== end) return Math.max(max, end - start)
+    return max
 };

48019f265b0466921ac35df76fbd71cc00d0559d
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-09-18 17:04:47 +0700

    Time: 68 ms (35.71%) | Memory: 44.6 MB (71.43%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.ts
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.ts
@@ -3,9 +3,10 @@ function kWeakestRows(mat: number[][], k: number): number[] {
     const colLength = mat[0].length;
     const weakest = new Set<number>();
     for(let i = 0; i <= colLength; i++) {
-        for(let k = 0; k < rowLength; k++) {
-            if(!mat?.[k]?.[i] && !weakest.has(k)) {
-                weakest.add(k)
+        for(let j = 0; j < rowLength; j++) {
+            if(!mat?.[j]?.[i] && !weakest.has(j)) {
+                weakest.add(j)
+                if(weakest.size === k) break;
             }
         }
     } 

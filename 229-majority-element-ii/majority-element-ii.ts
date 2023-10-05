a8e8eb830bee12f99005553d3a537e0c34d5dd03
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-10-05 07:09:15 +0700

    Time: 57 ms (82.46%) | Memory: 45.6 MB (49.12%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.ts
+++ 229-majority-element-ii/majority-element-ii.ts
@@ -1,13 +1,21 @@
 function majorityElement(nums: number[]): number[] {
-    const counter = new Map<number, number>();
-    const result = new Set<number>();
+    nums.sort((a,b)=>a-b);
+    const result = [];
     const third = nums.length / 3;
+    let counter = 1;
 
-    for(let i = 0; i < nums.length; i++) {
-        const newC = (counter.get(nums[i]) ?? 0) + 1;
-        counter.set(nums[i], newC);
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] === nums[i - 1]) counter++;
+        else {
+            if(counter > third) {
+                result.push(nums[i - 1]);
+            }
+            counter = 1;
+        }
+    }
 
-        if(newC > third) result.add(nums[i])
+    if(counter > third) {
+        result.push(nums[nums.length - 1]);
     }
 
     return Array.from(result)

f9b10fcecb3214cb8804b0d7540a5c3002a21487
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-10-26 07:55:12 +0700

    Time: 66 ms (75.41%) | Memory: 51.9 MB (8.41%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -0,0 +1,18 @@
+function maxArea(height: number[]): number {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        let currentArea = Math.min(height[left], height[right]) * (right - left);
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+};

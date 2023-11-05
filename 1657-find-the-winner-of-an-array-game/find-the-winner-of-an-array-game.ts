42b218eeae8f0e53988e64a7c5d42919be28f425
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-05 10:20:24 +0700

    Time: 67 ms (33.33%) | Memory: 52.4 MB (33.33%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.ts
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.ts
@@ -1,17 +1,16 @@
 function getWinner(arr: number[], k: number): number {
-    let curHighest = arr.shift();
+    let curHighest = 0;
     let counter = 0;
 
-    while(counter < k && counter < arr.length) {
-        if(curHighest > arr[0]) {
-            arr.push(arr.shift())
-            counter++
-        } else {
-            arr.push(curHighest);
-            curHighest = arr.shift();
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[curHighest] > arr[i]) counter++
+        else {
+            curHighest = i;
             counter = 1;
         }
+
+        if(counter === k) return arr[curHighest]
     }
 
-    return curHighest
+    return arr[curHighest]
 };

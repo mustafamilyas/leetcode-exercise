87805c051c872e6633dd6e28afee2e24b0dd89ed
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-08 09:16:10 +0700

    Time: 59 ms (75.00%) | Memory: 45.1 MB (6.94%) - LeetSync

---
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.ts
@@ -0,0 +1,9 @@
+function isReachableAtTime(sx: number, sy: number, fx: number, fy: number, t: number): boolean {
+    if(sx === fx && sy === fy) {
+        return t !== 1
+    }
+    const diffX = Math.abs(fx - sx) - 1;
+    const diffY = Math.abs(fy - sy) - 1;
+    const minTime = Math.max(diffX, diffY) + 1;
+    return t >= minTime;
+};

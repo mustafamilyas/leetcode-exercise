0b4bf8c80bf7edd93b39c28c84ec9156386ac86b
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-09 08:09:50 +0700

    Time: 74 ms (50.00%) | Memory: 46.9 MB (50.00%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.ts
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.ts
@@ -1,13 +1,11 @@
 function countHomogenous(s: string): number {
-    let start = 0;
-    let counter = 0;
+    let counter = 1;
+    let res = 0;
     for(let i = 1; i <= s.length; i++) {
-        if(s[start] !== s[i]) {
-            const length = i - start;
-            counter += length * (length + 1) / 2
-            start = i;
-        }
+        if(s[i - 1] === s[i]) counter ++
+        else counter = 1
+        res += counter;
     }
 
-    return counter % (10 ** 9 + 7);
+    return res % (10 ** 9 + 7);
 };

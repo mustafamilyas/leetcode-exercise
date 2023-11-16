16bc7667a620f4e8ea799e892b912357438310fd
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-16 09:34:42 +0700

    Time: 49 ms (90.00%) | Memory: 43.9 MB (50.00%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.ts
+++ 2107-find-unique-binary-string/find-unique-binary-string.ts
@@ -1,6 +1,8 @@
 function findDifferentBinaryString(nums: string[]): string {
     let result = '';
 
+    // we know that the length of each nums the same as nums length
+    // we can just take the diagonal and flip the bit to make a unique string
     for(let i = 0; i < nums.length; i++) {
         result += (nums[i][i] === '0') ? '1' : '0'
     }

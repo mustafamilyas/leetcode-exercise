b06ba01ff459494930518d17a3b7ab349e39e5db
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-20 09:07:23 +0700

    Time: 2119 ms (9.52%) | Memory: 71.2 MB (47.62%) - LeetSync

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.ts
@@ -0,0 +1,33 @@
+const GARBAGE_TYPES = ['M', 'P', 'G'];
+
+function garbageCollection(garbage: string[], travel: number[]): number {
+    const truckPosition = {M: 0, P: 0, G: 0};
+    let times = 0;
+
+    for(let i = 0; i < garbage.length; i++) {
+        times += garbage[i].length;
+        const garbageNum = getGarbageNum(garbage[i]);
+        if(garbageNum.M) moveGarbageTruck('M', i)
+        if(garbageNum.P) moveGarbageTruck('P', i)
+        if(garbageNum.G) moveGarbageTruck('G', i)
+        
+    }
+    return times;
+    /**********************************************************/
+    function getGarbageNum(garbage: string) {
+        const garbageObj = {M: 0, P: 0, G: 0}
+        for(let i = 0; i < garbage.length; i++) {
+            garbageObj[garbage[i]]++
+        }
+        return garbageObj;
+    }
+
+    function moveGarbageTruck(type: string, target: number) {
+        const startPosition = truckPosition[type] ?? 0;
+        for(let i = startPosition; i < target; i++) {
+            console.log('cost', travel[i])
+            times += travel[i]
+        }
+        truckPosition[type] = target;
+    }
+};

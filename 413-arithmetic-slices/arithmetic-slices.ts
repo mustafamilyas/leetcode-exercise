eb63cdea840bc2d5fe4a2792b42a1b2548779892
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-23 11:35:05 +0700

    Time: 52 ms (64.71%) | Memory: 42.7 MB (82.35%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.ts
+++ 413-arithmetic-slices/arithmetic-slices.ts
@@ -1,14 +1,12 @@
 function numberOfArithmeticSlices(nums: number[]): number {
     if(nums.length <= 2) return 0;
-    let currentDiff = nums[1] - nums[0];
     let counter = 0;
     let localCounter = 1;
 
     for(let i = 2; i < nums.length; i++) {
-        if(nums[i] - nums[i - 1] === currentDiff) {
+        if(nums[i] - nums[i - 1] === nums[i - 1] - nums[i - 2]) {
             counter += localCounter++
         } else {
-            currentDiff = nums[i] - nums[i - 1];
             localCounter = 1;
         }
     }

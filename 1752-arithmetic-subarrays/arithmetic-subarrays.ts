d1a58d899e7c56fda248af6981021aa94568f7d6
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-23 07:45:26 +0700

    Time: 62 ms (100.00%) | Memory: 50.7 MB (18.18%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.ts
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.ts
@@ -1,13 +1,22 @@
 function checkArithmeticSubarrays(nums: number[], l: number[], r: number[]): boolean[] {
-    return l.map((left, index)=>check(nums.slice(left, r[index] + 1)))
-};
+    return l.map((left, index)=>{
+        const right = r[index]
+        const localLength = right - left + 1
+        if(localLength <= 2) return true;
+        let max = nums[left], min = nums[left];
+        for(let i = left; i <= right; i++) {
+            max = Math.max(max, nums[i])
+            min = Math.min(min, nums[i])
+        }
+        const diff = (max - min) / (localLength - 1);
+        const checkArray = new Array(localLength).fill(false);
 
-function check(nums: number[]) {
-    if(nums.length <= 2) return true;
-    nums.sort((a,b)=>a-b)
-    const diff = nums[1] - nums[0];
-    for(let i = 2; i < nums.length; i++) {
-        if(diff !== nums[i] - nums[i - 1]) return false;
-    }
-    return true;
-}
+        for(let i = left; i <= right; i++) {
+            const localDiff = nums[i] - min;
+            if(localDiff % diff) return false;
+            checkArray[diff ? localDiff / diff : 0] = true;
+        }
+        const sequenceLength = checkArray.filter(i => i).length
+        return sequenceLength === 1 || sequenceLength === localLength;
+    })
+};

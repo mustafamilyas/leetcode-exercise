371311cb8104759f7cdeb0a142d06528a9383ee3
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-11-28 07:29:25 +0700

    Time: 128 ms (100.00%) | Memory: 55 MB (100.00%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.ts
@@ -0,0 +1,25 @@
+enum Furniture {
+  Seat = 'S',
+  Plant = 'P',
+}
+function numberOfWays(corridor: string): number {
+  let seatCounter = 0;
+  let plantCounter = 0;
+  let divider = 1;
+
+  for(const furniture of corridor) {
+    if(furniture === Furniture.Seat) {
+      if(seatCounter % 2 === 0) {
+        divider = (divider  * (plantCounter + 1)) % (1e9 + 7)
+        plantCounter = 0;
+      }
+      seatCounter++;
+    } else {
+      if(seatCounter >= 2 && seatCounter % 2 === 0) {
+        plantCounter++;
+      }
+    }
+  }
+
+  return seatCounter >= 2 && seatCounter % 2 === 0 ? divider : 0;  
+};

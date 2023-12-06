09d8f368a136d9bd9f0ba91c194fa6efeebc36fa
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-12-07 04:47:39 +0700

    Time: 13 ms (10.46%) | Memory: 2.8 MB (89.92%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.go
@@ -0,0 +1,24 @@
+func romanToInt(s string) int {
+    mapRomanToInt := map[string]int{
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    runS := []rune(s)
+    sLen := len(runS)
+    result := 0
+
+    for i := sLen - 1; i >= 0; i-- {
+        if i < sLen - 1 && mapRomanToInt[string(runS[i])] < mapRomanToInt[string(runS[i + 1])] {
+            result = result - mapRomanToInt[string(runS[i])]
+        } else {
+            result = result + mapRomanToInt[string(runS[i])]
+        }
+    }
+
+    return result;
+}

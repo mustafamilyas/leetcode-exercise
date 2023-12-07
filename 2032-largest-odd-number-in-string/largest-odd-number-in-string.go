171dce2edb04f2cf7cfea1a909dc36b656a2a56e
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-12-07 07:48:23 +0700

    Time: 10 ms (41.56%) | Memory: 6.4 MB (98.70%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.go
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.go
@@ -1,12 +1,7 @@
 func largestOddNumber(num string) string {
-    chars  := strings.Split(num, "")
-    for i := len(chars) - 1; i >= 0; i-- {
-      charNum, err := strconv.Atoi(chars[i])
-      if err != nil {
-        return ""
-      }
-      if (charNum % 2) == 1 {
-        return num[:i + 1]
+    for i := len(num) - 1; i >= 0; i-- {
+      if (num[i] - '0') % 2 != 0 {
+        return num[:i+1]
       }
     }
     return ""   

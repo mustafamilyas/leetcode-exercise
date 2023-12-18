83a957ca4aaab0ebc1c0b5c4ef3fc39621a506fb
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-12-18 13:48:20 +0700

    Time: 20 ms (77.92%) | Memory: 6.4 MB (64.94%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.go
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.go
@@ -1,20 +1,16 @@
 func maxProductDifference(nums []int) int {
-    min1 := 10000
-    min2 := 10000
-    max1 := 1
-    max2 := 1
+    min1, min2 := math.MaxInt, math.MaxInt
+    max1, max2 := math.MinInt, math.MinInt
 
     for _, value := range nums {
         if (value > max1) {
-            max2 = max1
-            max1 = value
+            max2, max1 = max1, value
         } else if (value > max2) {
             max2 = value
         }
 
         if (value < min1) {
-            min2 = min1
-            min1 = value
+            min2, min1 = min1, value
         } else if (value < min2) {
             min2 = value
         }

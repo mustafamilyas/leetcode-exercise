e2eef96fa9b9ebe3557208c575536bc1d6f5b55a
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2023-12-27 16:10:23 +0700

    Time: 102 ms (8.33%) | Memory: 53.7 MB (25.00%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.ts
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.ts
@@ -1,17 +1,17 @@
 function minCost(colors: string, neededTime: number[]): number {
-    let totalCost = 0, prevCostBeforeConsecutive = 0, globalCost = 0;
+    let validCost = 0, prevCostBeforeConsecutive = 0, totalCost = 0;
     for(let i = 0; i < colors.length; i++) {
-        globalCost += neededTime[i]
+        totalCost += neededTime[i]
         if(colors[i] === colors?.[i - 1]) {
-            totalCost = Math.max(
-                totalCost, 
+            validCost = Math.max(
+                validCost, 
                 prevCostBeforeConsecutive + neededTime[i]
             )
         } else {
-            totalCost += neededTime[i]
-            prevCostBeforeConsecutive = totalCost - neededTime[i]
+            validCost += neededTime[i]
+            prevCostBeforeConsecutive = validCost - neededTime[i]
         }
     }
 
-    return globalCost - totalCost;
+    return totalCost - validCost;
 };

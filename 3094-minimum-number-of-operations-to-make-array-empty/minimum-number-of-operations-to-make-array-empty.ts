8cf1c2a5ffd31a466d425bd0cf6cf67420ab2ad5
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-04 19:21:58 +0700

    Time: 98 ms (71.43%) | Memory: 54.9 MB (42.86%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.ts
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.ts
@@ -1,11 +1,11 @@
 function minOperations(nums: number[]): number {
-    const map: Record<number, number> = {};
+    const map = new Map<number, number>();
     for(const n of nums) {
-        if(map[n] == undefined) map[n] = 1
-        else map[n]++
+        if(!map.has(n)) map.set(n, 1)
+        else map.set(n, map.get(n) + 1)
     }
     let result = 0;
-    for(const values of Object.values(map)) {
+    for(const values of map.values()) {
         if(values === 1) return -1;
         result += Math.ceil(values / 3)
     }

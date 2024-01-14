39c2ece28738ff03c82c326e7bb11f2a3208bdfa
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-14 14:56:12 +0700

    Time: 73 ms (99.24%) | Memory: 50.8 MB (76.81%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
@@ -1,12 +1,15 @@
 function closeStrings(word1: string, word2: string): boolean {
-    return word1.length === word2.length && hash(word1) === hash(word2);
+    if (word1.length !== word2.length) return false;
+    const charCounter1 = createCharCounter(word1);
+    const charCounter2 = createCharCounter(word2);
+    for(let i = 0; i < 26; i++) {
+        if((charCounter1[i] > 0 && charCounter2[i] === 0) || (charCounter1[i] === 0 && charCounter2[i] > 0)) return false;
+    }
+    return charCounter1.sort((a,b)=>b-a).join(':') === charCounter2.sort((a,b)=>b-a).join(':')
 };
 
-function hash(s: string): string {
-    const count: Record<string, number> = {};
-    for(let i = 0; i < s.length; i++) {
-        if(count[s[i]]) count[s[i]]++
-        else count[s[i]] = 1
-    }
-    return Object.values(count).sort((a,b) => a-b).join(':') + ':' + Object.keys(count).sort().join(':')
+function createCharCounter(word: string) {
+    const count = new Array(26).fill(0);
+    for(const c of word) { count[c.charCodeAt(0) - 97]++ }
+    return count;
 }

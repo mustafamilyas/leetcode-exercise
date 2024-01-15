b0b3d5111e21df829ae450a97dba309fe9287b8e
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-15 07:48:53 +0700

    Time: 297 ms (95.58%) | Memory: 123.8 MB (36.28%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.ts
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.ts
@@ -1,18 +1,19 @@
 function findWinners(matches: number[][]): number[][] {
     const record = new Map<number, number>();
-    let start = Number.MAX_SAFE_INTEGER, end = Number.MIN_SAFE_INTEGER;
     for(const [winner, loser] of matches) {
         if(!record.has(winner)) record.set(winner, 0);
         if(!record.has(loser)) record.set(loser, 1);
         else record.set(loser, record.get(loser) + 1)
-        start = Math.min(start, winner, loser)
-        end = Math.max(end, winner, loser)
     }
     const result  = [[], []]
-    for(let key = start; key <= end; key++) {
-        const value = record.get(key)
-        if(value == 0) result[0].push(key)
-        else if(value == 1) result[1].push(key)
+    for(const [key, value] of record.entries()) {
+        if(value === 0) {
+            result[0].push(key)
+        } else if (value === 1) {
+            result[1].push(key)
+        }
     }
+    result[0].sort((a,b)=>a-b)
+    result[1].sort((a,b)=>a-b)
     return result;
 };

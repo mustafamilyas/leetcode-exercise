151337f3a4b6dd8f9190a37d1ac984be2d08e688
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-17 07:28:58 +0700

    Time: 60 ms (50.09%) | Memory: 45.3 MB (16.01%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.ts
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.ts
@@ -3,10 +3,6 @@ function uniqueOccurrences(arr: number[]): boolean {
     for(const value of arr) {
         map.set(value, (map.get(value) ?? 0) + 1)
     }
-    const set = new Set();
-    for(const value of map.values()) {
-        if(set.has(value)) return false;
-        set.add(value)
-    }
-    return true;
+    const set = new Set(map.values());
+    return map.size === set.size;
 };

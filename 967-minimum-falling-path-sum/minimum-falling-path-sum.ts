9d1d3e1e15f226e95c4a5fe0f34af12a6eefe01f
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-19 07:17:48 +0700

    Time: 61 ms (80.00%) | Memory: 45.5 MB (68.57%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.ts
@@ -0,0 +1,19 @@
+function minFallingPathSum(matrix: number[][]): number {
+    let min = Number.MAX_SAFE_INTEGER;
+    for(let i = 0; i < matrix.length; i++) {
+        for(let k = 0; k < matrix[i].length; k++) {
+            if(i > 0) {
+                matrix[i][k] += Math.min(
+                    (matrix[i - 1]?.[k - 1] ?? Number.MAX_SAFE_INTEGER), 
+                    matrix[i - 1][k], 
+                    (matrix[i - 1]?.[k + 1] ?? Number.MAX_SAFE_INTEGER
+                ))
+            }
+
+            if(i === matrix.length - 1) {
+                min = Math.min(matrix[i][k], min)
+            }
+        }
+    }
+    return min
+};

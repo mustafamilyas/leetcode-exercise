09ad51d9a43c5b425a65a6e375a2bccb39d5b6b5
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-25 07:20:55 +0700

    Time: 147 ms (33.11%) | Memory: 76.4 MB (32.08%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.ts
+++ 1250-longest-common-subsequence/longest-common-subsequence.ts
@@ -1,18 +1,16 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
-    const memo = new Array(text1.length)
-        .fill(0)
-        .map(()=> new Array(text2.length).fill(0));
+    const memo = new Array(text1.length + 1).fill([]).map(()=> new Array(text2.length + 1).fill(0))
     
-    let prev = 0;
-    for(let i = 0; i < text1.length; i++) {
-        for(let j = 0; j < text2.length; j++) {
+    for(let i = text1.length - 1; i >= 0; i--) {
+        for(let j = text2.length - 1; j >= 0; j--) {
             if(text1[i] === text2[j]) {
-                memo[i][j] = 1 + (memo?.[i - 1]?.[j - 1] ?? 0)
+                memo[i][j] = memo[i + 1][j + 1] + 1;
             } else {
-                memo[i][j] = Math.max(memo?.[i - 1]?.[j] ?? 0, memo?.[i]?.[j - 1] ?? 0)
+                memo[i][j] = Math.max(memo[i][j+1], memo[i+1][j])
             }
         }
     }
-    return memo[text1.length - 1][text2.length - 1]
+    
+    return memo[0][0]
 };
 

482b0f898d2290a65e73bfb1fd3c58820d514a9c
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-25 07:41:11 +0700

    Time: 103 ms (27.27%) | Memory: 54.7 MB (18.18%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.ts
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.ts
@@ -1,9 +1,9 @@
 function minDistance(word1: string, word2: string): number {
     const m = word1.length, n = word2.length;
     let prev = new Array(n).fill(0);
+    let cur = new Array(n).fill(0);
 
     for(let i = 0; i < m; i++) {
-        const cur = new Array(n).fill(0);
         for(let k = 0; k < n; k++) {
             if(word1[i] === word2[k]) {
                 cur[k] = (prev?.[k - 1] ?? 0) + 1;
@@ -11,7 +11,9 @@ function minDistance(word1: string, word2: string): number {
                 cur[k] = Math.max(prev[k], cur?.[k - 1] ?? 0)
             }
         }
+        const temp = prev;
         prev = cur;
+        cur = temp;
     }
     const similarWordLength = prev[n - 1]
     return m + n - 2 * similarWordLength

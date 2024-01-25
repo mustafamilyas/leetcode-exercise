56ab68916328b91d4c21dd885edbb8c34d973e0a
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-01-25 07:29:37 +0700

    Time: 112 ms (58.70%) | Memory: 57.1 MB (89.42%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.ts
+++ 1250-longest-common-subsequence/longest-common-subsequence.ts
@@ -1,16 +1,19 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
-    const memo = new Array(text1.length + 1).fill([]).map(()=> new Array(text2.length + 1).fill(0))
-    
-    for(let i = text1.length - 1; i >= 0; i--) {
-        for(let j = text2.length - 1; j >= 0; j--) {
-            if(text1[i] === text2[j]) {
-                memo[i][j] = memo[i + 1][j + 1] + 1;
+    const m = text1.length, n = text2.length;
+    let prev = new Array(n).fill(0);
+
+    for(let i = 0; i < m; i++) {
+        const cur = new Array(n).fill(0)
+        for(let k = 0; k < n; k++) {
+            if(text1[i] === text2[k]) {
+                cur[k] = (prev?.[k - 1] ?? 0) + 1;
             } else {
-                memo[i][j] = Math.max(memo[i][j+1], memo[i+1][j])
+                cur[k] = Math.max(cur?.[k - 1] ?? 0, prev[k])
             }
         }
+        prev = cur
     }
     
-    return memo[0][0]
+    return prev[n - 1]
 };
 

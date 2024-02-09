97ca3eb0c0ddc3e092763bfe433c50625e2f4f59
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-02-09 08:20:41 +0700

    Time: 94 ms (44.52%) | Memory: 54.3 MB (45.48%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.ts
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.ts
@@ -1,11 +1,8 @@
 function firstUniqChar(s: string): number {
     const lastSeen = {}
     for(let i = s.length - 1; i >= 0; i--) {
-        if(lastSeen[s[i]] !== undefined) {
-            lastSeen[s[i]] = -1;
-        } else {
-            lastSeen[s[i]] = i;
-        }
+        if(lastSeen[s[i]] !== undefined) lastSeen[s[i]] = -1;
+        else lastSeen[s[i]] = i;
     }
     for(let i = 0; i < s.length; i++) {
         if(lastSeen[s[i]] !== - 1) return lastSeen[s[i]]

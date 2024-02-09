198f7ada668e995b974feaa17fa5e92d6712553d
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-02-09 08:20:20 +0700

    Time: 79 ms (81.02%) | Memory: 54 MB (51.72%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.ts
@@ -0,0 +1,14 @@
+function firstUniqChar(s: string): number {
+    const lastSeen = {}
+    for(let i = s.length - 1; i >= 0; i--) {
+        if(lastSeen[s[i]] !== undefined) {
+            lastSeen[s[i]] = -1;
+        } else {
+            lastSeen[s[i]] = i;
+        }
+    }
+    for(let i = 0; i < s.length; i++) {
+        if(lastSeen[s[i]] !== - 1) return lastSeen[s[i]]
+    }
+    return -1
+};

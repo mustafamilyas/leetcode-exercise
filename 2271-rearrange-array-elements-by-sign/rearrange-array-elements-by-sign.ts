dbe938d560b9066f6608fc6c148e9461fe642728
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-02-14 20:31:56 +0700

    Time: 330 ms (28.38%) | Memory: 93.4 MB (78.38%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.ts
@@ -0,0 +1,9 @@
+function rearrangeArray(nums: number[]): number[] {
+    const res = [...nums]
+    let posIdx = -2, negIdx = -1;
+    for(const n of nums) {
+        if(n >= 0) res[posIdx+=2] = n;
+        else res[negIdx+=2] = n
+    }
+    return res;
+};

97523c49890e768f8e142b9e9ae640d69f5724b1
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-02-18 16:59:34 +0700

    Time: 482 ms (33.33%) | Memory: 84.6 MB (50.00%) - LeetSync

---
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.ts
@@ -0,0 +1,36 @@
+function mostBooked(n: number, meetings: number[][]): number {
+    meetings.sort((a, b)=>a[0] - b[0])
+    const room = new Array(n);
+    let maxIdx = 0;
+
+    for(let m = 0; m < meetings.length; m++) {
+        const [start, end] = meetings[m]
+        let soomEmptyRoomIdx = -1;
+        for(let i = 0; i < n; i++) {
+            if(room[i] == null) {
+                room[i] = [end];
+                soomEmptyRoomIdx = -1;
+                break;
+            }
+            const curRoom = room[i]
+            if(curRoom[curRoom.length - 1] <= start) {
+                curRoom.push(end);
+                if(room[maxIdx].length < curRoom.length || (room[maxIdx].length === curRoom.length && i < maxIdx)) maxIdx = i
+                soomEmptyRoomIdx = -1;
+                break;
+            }
+
+            const soonEmptyRoom = room[soomEmptyRoomIdx];
+            if(soomEmptyRoomIdx == -1 || curRoom[curRoom.length - 1] < soonEmptyRoom[soonEmptyRoom.length - 1]) {
+                soomEmptyRoomIdx = i;
+            }
+        }
+        if(soomEmptyRoomIdx > -1) {
+            const soonEmptyRoom = room[soomEmptyRoomIdx];
+            const nextStart = soonEmptyRoom[soonEmptyRoom.length - 1];
+            soonEmptyRoom.push(nextStart + end - start);
+            if(room[maxIdx].length < soonEmptyRoom.length || (room[maxIdx].length === soonEmptyRoom.length && soomEmptyRoomIdx < maxIdx)) maxIdx = soomEmptyRoomIdx
+        }
+    }
+    return maxIdx
+};

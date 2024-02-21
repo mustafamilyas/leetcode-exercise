01cc618409015a785cf555c5b8aaa9d8d378f45c
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-02-21 07:27:23 +0700

    Time: 130 ms (76.67%) | Memory: 57.3 MB (36.67%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.ts
@@ -0,0 +1,34 @@
+/**
+5 101  
+6 110
+7 111
+------
+4 100
+
+1  0001
+2  0010
+3  0011
+4  0100
+5  0101
+6  0110
+7  0111
+8  1000
+9  1001
+10 1010
+------
+4 100
+ */
+function rangeBitwiseAnd(left: number, right: number): number {
+    let res = 0, leftmostLeft = 1, leftmostRight = 1, counter = 0;
+    while(leftmostLeft === leftmostRight && leftmostLeft > 0) {
+        leftmostLeft = Math.floor(Math.log2(left))
+        leftmostRight = Math.floor(Math.log2(right))
+        if(leftmostLeft === leftmostRight) {
+            const leftmost = 2 ** leftmostRight
+            res += leftmost;
+            left -= leftmost;
+            right -= leftmost;
+        }
+    };
+    return res;
+};

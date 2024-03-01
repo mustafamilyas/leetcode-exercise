549fbcf711e5b4eb5c37fe3c6d923c514ff074a3
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-03-01 07:25:18 +0700

    Time: 89 ms (12.70%) | Memory: 53.3 MB (61.90%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.ts
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.ts
@@ -1,7 +1,4 @@
 function maximumOddBinaryNumber(s: string): string {
-    let oneCounter = 0;
-    for(const c of s) {
-        if(c === '1') oneCounter++
-    }
+    let oneCounter = s.split('').filter(c=>c === '1').length
     return '1'.repeat(oneCounter - 1) + '0'.repeat(s.length - oneCounter) + '1'.repeat(1)
 };

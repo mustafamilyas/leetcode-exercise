3646b27002db7ed3b5bc67a2d9c1c4eb1c2e8d0b
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-03-06 07:39:05 +0700

    Time: 73 ms (14.56%) | Memory: 51.9 MB (63.11%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    if(head == null) return head;
+    let nextHead = head;
+    let prev = null;
+    let cur = head;
+
+    while(cur) {
+        const prevBeforeChanges = prev;
+        const watchedNode = cur;
+        let counter = 0;
+        while(cur && cur.val === watchedNode.val) {
+            prev = cur;
+            cur = cur.next;
+            counter++;
+        }
+        if(counter > 1) {
+            if(prevBeforeChanges === null) {
+                nextHead = cur;
+                prev = null;
+            } else {
+                prevBeforeChanges.next = cur;
+                prev = prevBeforeChanges
+            }
+        }
+    }
+    return nextHead;
+};

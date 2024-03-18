4e3b113a167cbe5fef457dc20f7433fb4a3ac54e
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-03-18 07:51:41 +0700

    Time: 218 ms (62.13%) | Memory: 73.1 MB (73.19%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.ts
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.ts
@@ -1,13 +1,10 @@
 function findMinArrowShots(points: number[][]): number {
     points.sort((a,b)=>a[1]-b[1]);
-    let window = points[0];
+    let curEnd = points[0][1];
     let arrow = 1;
     for(let i = 1; i < points.length; i++) {
-        const curPoint = points[i]
-        if(curPoint[0] <= window[1]) {
-            window = [Math.max(curPoint[0], window[0]), Math.min(curPoint[1], window[1])]
-        } else {
-            window = points[i];
+        if(points[i][0] > curEnd) {
+            curEnd = points[i][1];
             arrow++
         }
     }

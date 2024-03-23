901e3c296aec76151b8e366be8de74eabb309a02
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-03-23 11:07:35 +0700

    Time: 91 ms (42.59%) | Memory: 61.2 MB (16.94%) - LeetSync

---
+++ 143-reorder-list/reorder-list.ts
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify head in-place instead.
+ */
+function reorderList(head: ListNode | null): void {
+    if(head === null || head.next === null) return;
+    
+    let slow = head;
+    let fast = head;
+    let prev = null;
+    
+    while(fast?.next) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next?.next;
+    }
+    
+    if(prev) {
+        prev.next = null;    
+    }
+    
+    let mid = reverse(slow, null);
+    let front = head;
+    
+    while(front !== null) {
+        if(front.next === null) {
+            front.next = mid;
+            break;
+        }
+        const frontNext = front.next;
+        const midNext = mid.next;
+        front.next = mid;
+        mid.next = frontNext;
+        front = frontNext;
+        mid = midNext;
+    }
+    
+};
+
+function reverse(node: ListNode | null, prevNode: ListNode | null): ListNode | null {
+    if(node === null) return prevNode;
+    
+    const temp = node.next;
+    node.next = prevNode;
+    
+    return reverse(temp, node)
+}

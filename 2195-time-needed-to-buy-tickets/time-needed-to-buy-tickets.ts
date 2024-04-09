d53729de240067ff3d2e474ee38970f7fc286891
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-04-09 07:51:36 +0700

    Time: 53 ms (81.82%) | Memory: 51.3 MB (45.45%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.ts
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.ts
@@ -1,17 +1,8 @@
 function timeRequiredToBuy(tickets: number[], k: number): number {
     let time = 0;
-    while(tickets[k]) {
-        let min = Number.MAX_SAFE_INTEGER;
-        for(const t of tickets) {
-            if(t) min = Math.min(t, min)
-        }
-        for(let i = 0; i < tickets.length; i++) {
-            if(tickets[i]) {
-                const resolvedMin = tickets[k] === 0 ? min - 1 : min
-                time += resolvedMin
-                tickets[i] -= resolvedMin
-            }
-        }
+    for(let i = 0; i < tickets.length; i++) {
+        if(i <= k) time += Math.min(tickets[i], tickets[k])
+        else       time += Math.min(tickets[i], tickets[k] - 1)
     }
     return time
 };

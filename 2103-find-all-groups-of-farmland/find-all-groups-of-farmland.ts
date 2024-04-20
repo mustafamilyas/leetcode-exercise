12fc2a60c37b7454f20f60a17eac23bd15d3eda6
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-04-20 18:18:35 +0700

    Time: 363 ms (20.00%) | Memory: 87.3 MB (30.00%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.ts
@@ -0,0 +1,24 @@
+const MOVEMENTS = [[0,1],[0,-1],[1,0],[-1,0]]
+function findFarmland(land: number[][]): number[][] {
+    const result = []
+    for(let i = 0; i < land.length; i++) {
+        for(let j = 0; j < land[i].length; j++) {
+            if(land[i][j] === 1) result.push(getCoordinate(i, j))
+        }
+    }
+    return result;
+
+    function getCoordinate(x: number, y: number) {
+        const bottomRight = [x, y];
+        const stack = [[x, y]];
+        while(stack.length) {
+            const [curx, cury] = stack.pop();
+            if(land?.[curx]?.[cury] !== 1) continue;
+            land[curx][cury] = -1;
+            bottomRight[0] = Math.max(bottomRight[0], curx)
+            bottomRight[1] = Math.max(bottomRight[1], cury)
+            stack.push(...MOVEMENTS.map(([dx, dy])=>[curx + dx, cury + dy]))
+        }
+        return [x, y, ...bottomRight]
+    }
+};

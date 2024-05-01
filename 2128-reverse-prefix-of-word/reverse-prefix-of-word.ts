c728fdad50bd2e16f8274c705f85c99551031e0b
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-05-01 17:17:52 +0700

    Time: 48 ms (90.68%) | Memory: 51.4 MB (67.80%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.ts
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.ts
@@ -1,8 +1,5 @@
 function reversePrefix(word: string, ch: string): string {
-    let i = 0, reversed = '';
-    for(; i < word.length && word[i] !== ch; i++){
-        reversed = word[i] + reversed
-    }
-    if(i === word.length) return word
-    return word[i] + reversed  + word.slice(i + 1)
+    const firstFound = word.indexOf(ch);
+    if(firstFound == -1) return word
+    return word.slice(0, firstFound + 1).split('').reverse().join('') + word.slice(firstFound + 1)
 };

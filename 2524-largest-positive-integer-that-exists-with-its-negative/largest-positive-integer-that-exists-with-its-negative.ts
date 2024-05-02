9af03fbfc66c8029a9e3d45f06922485fb2a4aa1
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-05-02 08:01:45 +0700

    Time: 67 ms (87.50%) | Memory: 54.5 MB (30.00%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.ts
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.ts
@@ -3,10 +3,9 @@ function findMaxK(nums: number[]): number {
     let max = -1;
     for(const n of nums) {
         const index = Math.abs(n) - 1;
-        if(counter[index] === 0) {
-            if(n > 0) counter[index] = 1
-            else counter[index] = 2
-        } else if((counter[index] === 1 && n < 0) || (counter[index] === 2 && n > 0)) counter[index] = 3
+        if(counter[index] === 0 && n > 0) counter[index] = 1
+        else if (counter[index] === 0) counter[index] = 2
+        else if((counter[index] === 1 && n < 0) || (counter[index] === 2 && n > 0)) counter[index] = 3
         if(counter[index] === 3) max = Math.max(max, Math.abs(n))
     }
     return max

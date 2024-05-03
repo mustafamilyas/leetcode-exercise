f81bb3716dd6547e14b3f17c01c712cbded4a21f
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-05-03 07:54:25 +0700

    Time: 50 ms (81.97%) | Memory: 50.1 MB (71.31%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.ts
+++ 165-compare-version-numbers/compare-version-numbers.ts
@@ -3,19 +3,21 @@ function compareVersion(version1: string, version2: string): number {
     const version2Array = versionTrimAndSplit(version2);
     const minLength = Math.min(version1Array.length, version2Array.length)
     for(let i = 0; i < minLength; i++) {
-        if(version1Array[i] > version2Array[i]) return 1
-        if(version1Array[i] < version2Array[i]) return -1
+        const a = parseInt(version1Array[i])
+        const b = parseInt(version2Array[i])
+        if(a > b) return 1
+        if(a < b) return -1
     }
     if(version1Array.length > version2Array.length) return 1;
     if(version1Array.length < version2Array.length) return -1;
     return 0
 };
 
-function versionTrimAndSplit(version: string): number[] {
-    const versionSplitted = version.split('.').map((e)=>parseInt(e));
+function versionTrimAndSplit(version: string): string[] {
+    const versionSplitted = version.split('.')
     let i = versionSplitted.length - 1;
     for(; i >= 0; i--) {
-        if(versionSplitted[i]) break;
+        if(parseInt(versionSplitted[i])) break;
     }
     return versionSplitted.slice(0, i + 1);
 }

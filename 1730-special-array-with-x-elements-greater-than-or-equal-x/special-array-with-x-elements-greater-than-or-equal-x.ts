72d450a63e027753eb7ad49633b037f55213e54d
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-05-27 10:00:06 +0700

    Time: 56 ms (88.24%) | Memory: 51.5 MB (82.35%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.ts
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.ts
@@ -1,7 +1,6 @@
 function specialArray(nums: number[]): number {
-    if(nums.length === 1) return nums[0] >= 1 ? 1 : -1
     nums.sort((a,b)=>b-a);
-    for(let i = 1; i < nums.length; i++) {
+    for(let i = 0; i < nums.length; i++) {
         if(nums[i] >= i + 1 && (i + 1 === nums.length || nums[i + 1] < i + 1)) return i + 1
     }
     return -1

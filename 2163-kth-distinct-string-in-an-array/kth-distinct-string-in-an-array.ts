a70a2253dd497d633113d047ed79d95c063da48d
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-06-01 11:08:07 +0700

    Time: 63 ms (76.19%) | Memory: 53.1 MB (59.52%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.ts
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.ts
@@ -1,15 +1,7 @@
 function kthDistinct(arr: string[], k: number): string {
-    const seen = new Set<string>();
-    const unique = new Set<string>();
-    for(const c of arr) {
-        if(unique.has(c)) {
-            unique.delete(c);
-        } else {
-            if(!seen.has(c)) {
-                seen.add(c)
-                unique.add(c)
-            }
-        }
-    }
-    return Array.from(unique)[k - 1] ?? ''
+    const counter = arr.reduce((acc, cur)=> {
+        acc[cur] = (acc[cur] ?? 0) + 1;
+        return acc
+    }, {})
+    return Object.keys(counter).filter(key=>counter[key] === 1)[k - 1] ?? ''
 };

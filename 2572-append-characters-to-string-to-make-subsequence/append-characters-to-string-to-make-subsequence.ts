3d221d2027c8e6ca09bbd012b0cdbc8ce7c85a01
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-06-03 07:11:10 +0700

    Time: 60 ms (61.11%) | Memory: 53.6 MB (44.44%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.ts
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.ts
@@ -1,8 +1,7 @@
 function appendCharacters(s: string, t: string): number {
     let tI = 0;
-    for(let sI = 0; sI < s.length; sI++) {
+    for(let sI = 0; sI < s.length && tI < t.length; sI++) {
         if(s[sI] === t[tI]) tI++
-        if(tI === t.length) break;
     }
     return t.length - tI
 };

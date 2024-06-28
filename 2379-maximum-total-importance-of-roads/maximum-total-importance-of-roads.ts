66c1826caff2c0d1a7a021f88aff2c211cdfc304
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-06-28 08:26:57 +0700

    Time: 177 ms (85.71%) | Memory: 76.7 MB (85.71%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.ts
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.ts
@@ -1,17 +1,13 @@
 function maximumImportance(n: number, roads: number[][]): number {
-    const map = new Map<number, number>();
-    for(const [from, to] of roads) {
-        map.set(from, (map.get(from) ?? 0) + 1)
-        map.set(to, (map.get(to) ?? 0) + 1)
-    }
-    const city = Array.from(map.entries()).sort((a, b)=>a[1] - b[1])
-    const cityImportance = new Array(city.length)
-    for(let i = 0; i < city.length; i++) {
-        cityImportance[city[i][0]] = i + 1 + (n - city.length)
-    }
+    const counter = new Array(n).fill(0);
     let sum = 0;
     for(const [from, to] of roads) {
-        sum += cityImportance[from] + cityImportance[to]
+        counter[from]++
+        counter[to]++
+    }
+    counter.sort((a,b)=>b-a);
+    for(let i = 0; i < n && counter[i] > 0; i++) {
+        sum += counter[i] * (n - i)
     }
     return sum;
 };

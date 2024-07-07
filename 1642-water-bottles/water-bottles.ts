6a1fb0c93359797b7bad758531e2255307470b3e
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-07-07 07:53:51 +0700

    Time: 61 ms (33.33%) | Memory: 50.1 MB (55.56%) - LeetSync

--- 1642-water-bottles/water-bottles.ts
+++ 1642-water-bottles/water-bottles.ts
@@ -1,10 +1,3 @@
 function numWaterBottles(numBottles: number, numExchange: number): number {
-    let drink = 0, empty = 0;
-    while(numBottles > 0) {
-        drink += numBottles;
-        empty += numBottles;
-        numBottles = Math.trunc(empty / numExchange)
-        empty %= numExchange
-    }
-    return drink;
+    return numBottles + Math.trunc((numBottles - 1) / (numExchange - 1))
 };

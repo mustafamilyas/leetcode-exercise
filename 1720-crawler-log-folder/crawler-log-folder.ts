9c2f8491fde0dce853364faef7d19b603578e6d1
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-07-10 08:05:55 +0700

    Time: 64 ms (32.31%) | Memory: 51.4 MB (84.62%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.ts
+++ 1720-crawler-log-folder/crawler-log-folder.ts
@@ -1,9 +1,9 @@
 function minOperations(logs: string[]): number {
     let step = 0;
-    for(const command of logs) {
-        if(command === './') continue;
-        if(command === '../' && step > 0) step--
-        if(command !== '../') step++
+    for(let i = 0; i < logs.length; i++) {
+        if(logs[i] === './') continue;
+        if(logs[i] === '../' && step > 0) step--
+        if(logs[i] !== '../') step++
     }
     return step;
 };

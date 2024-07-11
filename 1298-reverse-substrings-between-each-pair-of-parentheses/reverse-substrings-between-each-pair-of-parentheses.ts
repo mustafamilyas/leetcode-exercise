e359943e6a3afff0bf33017ef7f72e545f509730
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-07-11 07:58:09 +0700

    Time: 57 ms (50.00%) | Memory: 51.7 MB (62.50%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.ts
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.ts
@@ -1,40 +1,20 @@
 function reverseParentheses(s: string): string {
-    let level = 0, cur = '';
-    const stack = [];
-    for(let i = 0; i < s.length; i++) {
-        if(s[i] === '(') {
-            stack.push([cur, level++])
-            cur = ''
-        } else if (s[i] === ')') {
-            if(level % 2) {
-                cur = reverse(cur)
-                while(stack.length && stack[stack.length - 1][1] >= level) {
-                    const [str, curLevel] = stack.pop();
-                    if(curLevel % 2) cur += reverse(str)
-                    else cur += str
-                }
-                stack.push([cur, level - 1])
-            } else {
-                while(stack.length && stack[stack.length - 1][1] >= level) {
-                    const [str, curLevel] = stack.pop();
-                    if(curLevel % 2) cur = reverse(str) + cur
-                    else cur = str + cur
-                }
-                stack.push([cur, level])
-            }
-            level--
-            cur = ''
-        } else {
-            cur += s[i];
+    let stack = [], res = "", chars = s.split("");
+
+    for(let i = 0; i < s.length ; i++){
+        if(s[i] === '(')
+            stack.push(i);
+        else if(s[i] === ')'){
+            const lastIndex = stack.pop();
+            reverse(chars, lastIndex+1, i-1); 
         }
     }
-    return stack.map((item)=>item[0]).join('') + cur
+    return chars.filter((ch) => ch !== '(' && ch !== ')').join('');
 };
 
-function reverse(s: string) {
-    let res = ''
-    for(let i = s.length - 1; i >= 0; i--) {
-        res += s[i]
+function reverse(arr: any[], l: number, r: number) {
+    while(l < r){
+        [arr[l], arr[r]] = [arr[r], arr[l]]
+        l++, r--
     }
-    return res;
 }

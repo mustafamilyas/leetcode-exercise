d48622e04da408d89413a1d0dc02fabef1514fc2
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-07-20 19:07:12 +0700

    Time: 226 ms (30.00%) | Memory: 67.4 MB (60.00%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.ts
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.ts
@@ -1,31 +1,12 @@
 function restoreMatrix(rowSum: number[], colSum: number[]): number[][] {
     const result = new Array(rowSum.length).fill(null).map(()=>new Array(colSum.length).fill(0))
-    const curRowSum = new Array(rowSum.length).fill(0);
-    const curColSum = new Array(colSum.length).fill(0);
-    backtrack(0, 0);
-    return result;
-    // ============================== UTILS ==============================
-    function backtrack(row: number, col: number): boolean {
-        if(row === rowSum.length) return true;
-        const maxRowNum = rowSum[row] - curRowSum[row]
-        const maxColNum = colSum[col] - curColSum[col]
-        if(col === colSum.length - 1 && maxRowNum >  maxColNum) return false;
-        if(row === rowSum.length - 1 && maxColNum >  maxRowNum) return false;
-        const maxAvailableNum = Math.min(maxRowNum, maxColNum)
-        let nextCol = col + 1, nextRow = row;
-        if(nextCol === colSum.length) {
-            nextCol = 0;
-            nextRow++
+    for(let i = 0; i < rowSum.length; i++) {
+        for(let k = 0; k < colSum.length; k++) {
+            const maxValue = Math.min(rowSum[i], colSum[k]);
+            result[i][k] = maxValue;
+            rowSum[i] -= maxValue;
+            colSum[k] -= maxValue;
         }
-        for(let num = maxAvailableNum; num >= 0; num--) {
-            result[row][col] = num;
-            curRowSum[row] += num
-            curColSum[col] += num
-            if(backtrack(nextRow, nextCol)) return true;
-            result[row][col] = 0
-            curRowSum[row] -= num
-            curColSum[col] -= num
-        }
-        return false;
     }
+    return result;
 };

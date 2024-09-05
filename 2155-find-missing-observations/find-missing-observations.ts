cef3bcf17643f078c9d98eee4417891dd353b4b9
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-09-05 08:11:42 +0700

    Time: 251 ms (100.00%) | Memory: 70.4 MB (50.00%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.ts
+++ 2155-find-missing-observations/find-missing-observations.ts
@@ -2,6 +2,8 @@ function missingRolls(rolls: number[], mean: number, n: number): number[] {
     const curSum = rolls.reduce((a,b)=>a + b, 0);
     const goalSum = (rolls.length + n) * mean
     let nSum = goalSum - curSum;
+    // nSum < n means that even with the lowest value (1), we can't fill the nArray
+    // nSum > n * 6 means that even with the highest value (6), we can't fill the nArray
     if(nSum < n || nSum > n * 6) return []
     const nArray = [];
     for(let i = 0; i < n; i++) {

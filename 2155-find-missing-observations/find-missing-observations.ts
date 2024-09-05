f2dcf1e505e76fe9ccc01b4b10cb0143552cd7bf
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-09-05 08:01:34 +0700

    Time: 236 ms (100.00%) | Memory: 72.8 MB (50.00%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.ts
@@ -0,0 +1,32 @@
+function missingRolls(rolls: number[], mean: number, n: number): number[] {
+    let diff = 0;
+    for(let i = 0; i < rolls.length; i++) {
+        diff += rolls[i] - mean;
+    }
+    if(diff === 0) return new Array(n).fill(mean)
+    const result = []
+    console.log(diff)
+    if(diff > 0) {
+        const maxDiff = mean - 1;
+        let avgInc = diff / n;
+        if(avgInc > maxDiff) return []
+        for(let i = 0; i < n; i++) {
+            avgInc = diff / (n - i)
+            const curValue = Math.ceil(avgInc)
+            result.push(mean - curValue)
+            diff -= curValue
+        }
+    } else if (diff < 0) {
+        diff *= -1
+        const maxDiff = 6 - mean;
+        let avgInc = diff / n;
+        if(avgInc > maxDiff) return []
+        for(let i = 0; i < n; i++) {
+            avgInc = diff / (n - i)
+            const curValue = Math.ceil(avgInc)
+            result.push(curValue + mean)
+            diff -= curValue
+        }
+    }
+    return result;
+};

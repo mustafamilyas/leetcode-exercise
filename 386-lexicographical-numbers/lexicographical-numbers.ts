6cb3071162b6049dfb3b69147bf78304c9883f71
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-09-21 20:41:15 +0700

    Time: 76 ms (58.33%) | Memory: 58.4 MB (66.67%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.ts
+++ 386-lexicographical-numbers/lexicographical-numbers.ts
@@ -1,19 +1,14 @@
 function lexicalOrder(n: number): number[] {
     const result = [];
-    let cur = 1;
-    while(result.length < n) {
-        result.push(cur)
-        cur = getNext(cur)
-    }
+    getNext(1)
     return result;
 
     function getNext(num: number) {
-        if(num * 10 <= n) return num * 10;
-        let nextNum = num + 1;
-        if(nextNum > n) nextNum = Math.ceil(nextNum / 10)
-        while(nextNum % 10 === 0) {
-            nextNum /= 10;
+        for(let i = 0; i < 10; i++) {
+            const cur = num + i;
+            if(cur > n || result.length === n) return;
+            result.push(cur)
+            getNext(cur * 10)
         }
-        return nextNum
     }
 };

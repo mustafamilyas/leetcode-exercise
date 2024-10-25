70a33a6541a64e2ff62b57fd10858602683288b5
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-10-25 07:46:38 +0700

    Time: 173 ms (50.00%) | Memory: 77.9 MB (33.33%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.ts
@@ -0,0 +1,63 @@
+function removeSubfolders(folder: string[]): string[] {
+    const trie = new Trie();
+    for(let i = 0; i < folder.length; i++) {
+        trie.insert(folder[i])
+    }
+    return trie.toArray()
+};
+
+class Node {
+    marked: boolean;
+    children: Record<string, Node>;
+    constructor() {
+        this.marked = false;
+        this.children = {}
+    }
+}
+
+class Trie {
+    root: Node;
+    constructor() {
+        this.root = new Node();
+    }
+
+    insert(str: string) {
+        const paths = str.split('/').slice(1)
+        let current = this.root;
+        let i = 0;
+        for(;i < paths.length && current.children[paths[i]] && !current.marked; i++) {
+            current = current.children[paths[i]]
+        }
+        if(i === paths.length || current.marked) {
+            current.marked = true
+            current.children = {}
+            return;
+        }
+        for(; i < paths.length; i++) {
+            const node = new Node();
+            current.children[paths[i]] = node;
+            current = node
+        }
+        current.marked = true
+    }
+
+    toArray() {
+        return toPath(this.root).map((e)=>'/' + e)
+    }
+}
+
+function toPath(trie: Node): string[] {
+    const keys = Object.keys(trie.children)
+    const result = []
+    for(const k of keys) {
+        const children = toPath(trie.children[k])
+        if(children.length) {
+            for(const c of children) {
+                result.push(`${k}/${c}`)
+            }
+        } else {
+            result.push(k)
+        }
+    }
+    return result;
+}

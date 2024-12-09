08c4ba439a7da1b58d64d3151e45efc3bee74bbf
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-12-09 07:53:23 +0700

    Time: 874 ms (60.00%) | Memory: 83.1 MB (50.00%) - LeetSync

--- 3427-special-array-ii/special-array-ii.ts
+++ 3427-special-array-ii/special-array-ii.ts
@@ -4,7 +4,6 @@ function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
         if(nums[i] % 2 == nums[i - 1] % 2) pattern += '0'
         else pattern += '1'
     }
-    console.log(pattern)
     return queries.map(([s, e])=> {
         const cur = pattern.slice(s, e + 1);
         return cur === '1'.repeat(e - s + 1) || cur === '0' + '1'.repeat(e - s) 

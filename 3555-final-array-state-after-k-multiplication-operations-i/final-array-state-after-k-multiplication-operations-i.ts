dc4df515faaf931d350c48536ad1ab70a859baa5
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2024-12-16 07:55:01 +0700

    Time: 1 ms (82.86%) | Memory: 52.3 MB (94.74%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.ts
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.ts
@@ -1,25 +1,8 @@
 function getFinalState(nums: number[], k: number, multiplier: number): number[] {
-    const counter: Record<number, number[]> = {};
-    const numbersQueue = new MinPriorityQueue();
-    for(let i = 0; i < nums.length; i++) {
-        addNumber(nums[i], i)
-    }
     for(let i = 0; i < k; i++) {
-        const node = numbersQueue.front();
-        const idx = counter?.[node.element]?.shift();
-        if(!counter?.[node.element]?.length) numbersQueue.dequeue()
-        nums[idx ?? 0] *= multiplier
-        addNumber(node.element * multiplier, idx ?? 0)
+        const min = Math.min(...nums);
+        const idx = nums.findIndex((v)=> v === min);
+        nums[idx] *= multiplier
     }
     return nums;
-    function addNumber(n: number, i: number) {
-        if(counter[n] == null || counter[n].length == 0) {
-            counter[n] = [i] 
-            numbersQueue.enqueue(n)
-        } else {
-            const idx = counter[n].findIndex((v)=>v >= i)
-            if(idx === -1) counter[n].push(i)
-            else counter[n].splice(idx, 0, i)
-        }
-    }
 };

79462aa9fc1a1fbe1e56a8ad0a7d22e7027ad05a
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-01-02 07:33:33 +0700

    Time: 23 ms (9.09%) | Memory: 76.5 MB (72.73%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.ts
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.ts
@@ -4,11 +4,15 @@ const isSpecial = (word: string) => isVowels(word[0]) && isVowels(word[word.leng
 function vowelStrings(words: string[], queries: number[][]): number[] {
     const counter = [];
     let sum = 0;
+    const specialWordSet = new Set<string>();
     for(let i = 0; i < words.length; i++) {
-        if(isSpecial(words[i])) sum++
+        if(isSpecial(words[i])) {
+            sum++
+            specialWordSet.add(words[i])
+        }
         counter.push(sum)
     }
-    return queries.map(([start, end]) => isSpecial(words[start]) ? counter[end] - counter[start] + 1 : counter[end] - counter[start] )
+    return queries.map(([start, end]) => specialWordSet.has(words[start]) ? counter[end] - counter[start] + 1 : counter[end] - counter[start] )
 };
 
 // 1 1 2 3 4

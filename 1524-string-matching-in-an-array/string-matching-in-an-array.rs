990b0febdb1e8f1a2e845d263c0dc32c1cee523f
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-01-07 08:03:49 +0700

    Time: 0 ms (100.00%) | Memory: 2.4 MB (21.43%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.rs
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.rs
@@ -1,13 +1,10 @@
 impl Solution {
     pub fn string_matching(words: Vec<String>) -> Vec<String> {
         let mut vec: Vec<String> = Vec::new();
-        let mut sorted_words: Vec<String> = Vec::new();
-        sorted_words = words.clone();
-        sorted_words.sort_by(|a, b| b.len().cmp(&a.len()));
-        for i in (1..sorted_words.len()) {
-            for k in (0..i) {
-                if sorted_words[k].contains(&sorted_words[i]) {
-                    vec.push(sorted_words[i].clone());
+        for i in (0..words.len()) {
+            for k in (0..words.len()) {
+                if words[k].contains(&words[i]) && i != k {
+                    vec.push(words[i].clone());
                     break;
                 };
             };

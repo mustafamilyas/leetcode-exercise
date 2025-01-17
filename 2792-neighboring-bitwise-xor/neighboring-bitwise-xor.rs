a67d2c2899537920bf70b59fbda12e32f84d19d3
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-01-17 07:58:46 +0700

    Time: 0 ms (100.00%) | Memory: 2.8 MB (50.00%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.rs
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.rs
@@ -1,35 +1,9 @@
 impl Solution {
     pub fn does_valid_array_exist(derived: Vec<i32>) -> bool {
-        for initial_value in 0..2 {
-            let mut original: Vec<i32> = vec![0; derived.len()];
-            original[0] = initial_value;
-            for _i in 0..original.len() {
-                let i = _i as usize; 
-                let next_idx = if i < derived.len() - 1 {
-                    i + 1
-                } else {
-                    0
-                };
-                let mut next_value = 0;
-                if derived[i] == 0 {
-                    if original[i] == 0 {
-                        next_value = 0;
-                    } else {
-                        next_value = 1;
-                    }
-                } else {
-                    if original[i] == 0 {
-                        next_value = 1;
-                    } else {
-                        next_value = 0;
-                    }
-                }
-                if i == derived.len() - 1 && original[0] == next_value {
-                    return true;
-                }
-                original[next_idx] = next_value;
-            }
+        let mut sum = 0;
+        for v in derived {
+            sum += v;
         }
-        false
+        sum % 2 == 0
     }
 }

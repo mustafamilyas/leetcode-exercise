496f206fc4adcb8cc1394cadae7f171735de6aa0
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-02-19 07:56:16 +0700

    Time: 0 ms (100.00%) | Memory: 57.6 MB (8.33%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.ts
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.ts
@@ -9,14 +9,12 @@ function getHappyString(n: number, k: number): string {
         if(i == 0) {
             totalCombination /= 3;
             idx = Math.floor(k / totalCombination);
-            res += char[idx];
-            k %= totalCombination;
         } else {
             totalCombination /= 2;
             idx = resolveIdx(Math.floor(k / totalCombination), prev);
-            res += char[idx];
-            k %= totalCombination;
         }
+        res += char[idx];
+        k %= totalCombination;
         prev = idx;
     }
     return res;

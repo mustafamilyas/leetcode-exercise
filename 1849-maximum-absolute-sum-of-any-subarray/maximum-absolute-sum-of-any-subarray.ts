284bb6574f570ae402ce6d8f9a30c2759a9cab8a
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-02-26 08:20:01 +0700

    Time: 0 ms (100.00%) | Memory: 63.2 MB (33.33%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.ts
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.ts
@@ -1,26 +1,11 @@
 function maxAbsoluteSum(nums: number[]): number {
-    const rightSum = new Array(nums.length + 1);
-    rightSum[nums.length] = [0, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER] // sum, max, min
-    for(let i = nums.length - 1; i >= 0; i--) {
-        rightSum[i] = [rightSum[i + 1][0] + nums[i], 0, 0] 
-        rightSum[i][1] = Math.max(rightSum[i + 1][1], rightSum[i][0])
-        rightSum[i][2] = Math.min(rightSum[i + 1][2], rightSum[i][0])
+    let sum = 0, min = 0, max = 0;
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        min = Math.min(min, sum)
+        max = Math.max(max, sum)
     }
-    let max = Math.max(...rightSum[0].map(Math.abs));
-    const total = rightSum[0][0];
-    const leftSum = [0, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]; // sum, max, min
-    for(let i = 0; i < nums.length - 1; i++) {
-        leftSum[0] += nums[i];
-        leftSum[1] = Math.max(leftSum[1], leftSum[0])
-        leftSum[2] = Math.min(leftSum[2], leftSum[0])
-        max = Math.max(
-            max, 
-            Math.abs(leftSum[0]), 
-            Math.abs((leftSum[1] + rightSum[i + 1][1]) - total), 
-            Math.abs(total - (leftSum[2] + rightSum[i + 1][2]))
-        )
-    }
-    return max;
+    return max - min
 };
 
 // 1, -3, 2, 3, -4

fd82acc39efa7f783eb64324ed3bc35c0560cf30
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-02-27 08:20:02 +0700

    Time: 421 ms (0.00%) | Memory: 98.3 MB (0.00%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.ts
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.ts
@@ -1,16 +1,17 @@
 function lenLongestFibSubseq(arr: number[]): number {
     let longest = 0;
-    const counter = new Map<string, number>()
-    const set = new Set(arr);
+    const counter = new Array(arr.length).fill(null).map(()=>new Array(arr.length).fill(2));
+    const valToIdx = new Map(arr.map((e, i)=>[e, i]));
     for(let i = 1; i < arr.length - 1; i++) {
         for(let k = i + 1; k < arr.length; k++) {
             const prev = arr[k] - arr[i];
-            if(!set.has(prev) || prev >= arr[i]) continue;
-            const prevKey = `${prev}:${arr[i]}`;
-            const prevValue = counter.get(prevKey) ?? 2
-            const nextKey = `${arr[i]}:${arr[k]}`;
+            const prevIdx = valToIdx.get(prev)
+            if(prevIdx == null || prev >= arr[i]) continue;
+            const curIdx = valToIdx.get(arr[i])
+            const nextIdx = valToIdx.get(arr[k])
+            const prevValue = counter[prevIdx][curIdx]
             const nextValue = prevValue + 1
-            counter.set(nextKey, nextValue)
+            counter[curIdx][nextIdx] = nextValue
             longest = Math.max(longest, nextValue)
         }
     }

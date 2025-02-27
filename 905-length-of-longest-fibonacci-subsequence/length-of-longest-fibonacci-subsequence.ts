8e846b2b400370157e63a87ffe879a8ef593f69a
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-02-27 08:11:27 +0700

    Time: 290 ms (50.00%) | Memory: 64.9 MB (0.00%) - LeetSync

---
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.ts
@@ -0,0 +1,17 @@
+function lenLongestFibSubseq(arr: number[]): number {
+    let longest = 0;
+    const counter = {};
+    const set = new Set(arr);
+    for(let i = 1; i < arr.length - 1; i++) {
+        for(let k = i + 1; k < arr.length; k++) {
+            const prev = arr[k] - arr[i];
+            if(!set.has(prev) || prev >= arr[i]) continue;
+            const key = `${prev}:${arr[i]}`;
+            if(counter[key] == null) counter[key] = 2
+            const nextKey = `${arr[i]}:${arr[k]}`;
+            counter[nextKey] = counter[key] + 1
+            longest = Math.max(longest, counter[nextKey])
+        }
+    }
+    return longest
+};

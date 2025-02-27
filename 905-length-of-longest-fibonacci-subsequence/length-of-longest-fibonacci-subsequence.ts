8b7598829bd09d0600994f0f5695eefcd2dbb375
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-02-27 08:13:31 +0700

    Time: 283 ms (50.00%) | Memory: 60.5 MB (0.00%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.ts
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.ts
@@ -1,16 +1,16 @@
 function lenLongestFibSubseq(arr: number[]): number {
     let longest = 0;
-    const counter = {};
+    const counter = new Map<string, number>()
     const set = new Set(arr);
     for(let i = 1; i < arr.length - 1; i++) {
         for(let k = i + 1; k < arr.length; k++) {
             const prev = arr[k] - arr[i];
             if(!set.has(prev) || prev >= arr[i]) continue;
             const key = `${prev}:${arr[i]}`;
-            if(counter[key] == null) counter[key] = 2
+            if(!counter.has(key)) counter.set(key, 2)
             const nextKey = `${arr[i]}:${arr[k]}`;
-            counter[nextKey] = counter[key] + 1
-            longest = Math.max(longest, counter[nextKey])
+            counter.set(nextKey, counter.get(key) + 1)
+            longest = Math.max(longest, counter.get(nextKey))
         }
     }
     return longest

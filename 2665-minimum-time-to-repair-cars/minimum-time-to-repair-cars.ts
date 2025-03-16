fee133c7a500f96f15aff5b70b6bebfd7bbb2745
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-03-17 06:03:52 +0700

    Time: 54 ms (25.00%) | Memory: 65.3 MB (0.00%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.ts
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.ts
@@ -1,5 +1,5 @@
 function repairCars(ranks: number[], cars: number): number {
-    let l = 1, r = 100 * cars * cars
+    let l = 1, r = Math.max(...ranks) * cars * cars
     while(l < r) {
         const mid = Math.floor((l + r) / 2);
         if(isAllRepaired(mid)) {

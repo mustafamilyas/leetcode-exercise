f21a312277e99d81b016836d7e8b78ae9d1b6851
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-04-17 08:16:47 +0700

    Time: 0 ms (100.00%) | Memory: 2.2 MB (37.50%) - LeetSync

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let n = nums.len();
+        let mut res = 0;
+        for i in 0..n - 1 {
+            for j in i + 1..n {
+                if (i * j) % k as usize == 0 && nums[i] == nums[j] {
+                    res += 1;
+                }
+            }
+        }
+        res
+    }
+}

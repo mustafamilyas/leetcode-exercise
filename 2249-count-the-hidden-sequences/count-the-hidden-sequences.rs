73674bbca6c7c6dd1d5cc2ba5dfe3c356b1899f4
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-04-21 08:09:23 +0700

    Time: 0 ms (100.00%) | Memory: 3.7 MB (87.50%) - LeetSync

---
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn number_of_arrays(differences: Vec<i32>, lower: i32, upper: i32) -> i32 {
+        let mut lowest = 0i32;
+        let mut highest = 0i32;
+        let mut current = 0i32;
+        for v in differences.iter() {
+            current += v;
+            lowest = std::cmp::min(current, lowest);
+            highest = std::cmp::max(current, highest);
+        }
+        match highest.checked_sub(lowest) {
+            Some(result) => std::cmp::max(0, upper - lower + 1 - (highest - lowest)),
+            None => 0 
+        }        
+    }
+}

76048ba1c16a7a5037934cecc1829dc6dbff7ba0
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-08-09 10:08:17 +0700

    Time: 0 ms (100.00%) | Memory: 2.2 MB (57.27%) - LeetSync

--- 231-power-of-two/power-of-two.rs
+++ 231-power-of-two/power-of-two.rs
@@ -1,8 +1,5 @@
 impl Solution {
     pub fn is_power_of_two(n: i32) -> bool {
-        if n <= 0 { return false }
-        let pow = n.ilog2();
-        let a = (2i32).pow(pow);
-        a & n == n
+        n > 0 && (n & (n - 1)) == 0
     }
 }

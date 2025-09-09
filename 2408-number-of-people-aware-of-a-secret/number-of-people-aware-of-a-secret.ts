77e5e119cd1b5dad03a62a503c35f0b287c0d22c
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-09-09 09:34:30 +0700

    Time: 9 ms (27.27%) | Memory: 60.9 MB (9.09%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.ts
@@ -0,0 +1,14 @@
+function peopleAwareOfSecret(n: number, delay: number, forget: number): number {
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    let share = 0n;
+    const MOD = 1000000007n;
+    for (let t = 2; t <= n; t++) {
+        if (t - delay > 0) share = (share + BigInt(dp[t - delay]) + MOD) % MOD;
+        if (t - forget > 0) share = (share - BigInt(dp[t - forget]) + MOD) % MOD;
+        dp[t] = Number(share);
+    }
+    let know = 0n;
+    for (let i = n - forget + 1; i <= n; i++) know = (know + BigInt(dp[i])) % MOD;
+    return Number(know);
+};

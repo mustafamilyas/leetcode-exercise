11ea79aa7ce248604d92b6693764dfdb5126378f
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-10-27 07:10:18 +0700

    Time: 12 ms (100.00%) | Memory: 63.5 MB (100.00%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.ts
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.ts
@@ -1,15 +1,13 @@
 function numberOfBeams(bank: string[]): number {
-    let total = 0;
     let prev = 0;
-
+    let sum = 0;
     for(const row of bank) {
-        let cctvs = 0
-        for(const col of row) {
-            cctvs += parseInt(col)
+        let cctv = 0;
+        for(let i = 0; i < row.length; i++) {
+            if(row[i] == '1') cctv +=1;
         }
-        if(prev >= 0 && cctvs == 0) continue;
-        if(prev >= 0) total += prev * cctvs
-        prev = cctvs;
+        sum += cctv * prev;
+        if(cctv > 0) prev = cctv;
     }
-    return total;
+    return sum;
 };

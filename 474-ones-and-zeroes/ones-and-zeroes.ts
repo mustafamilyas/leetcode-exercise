7455f492d9d81340934c2e68a579a75c078f1c55
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-11-12 06:44:09 +0700

    Time: 61 ms (86.84%) | Memory: 61.1 MB (60.53%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.ts
+++ 474-ones-and-zeroes/ones-and-zeroes.ts
@@ -1,12 +1,18 @@
 function findMaxForm(strs: string[], m: number, n: number): number {
-    let dp = Array.from({ length: m + 1 }, () => new Uint8Array(n + 1))
-    for (let i = 0; i < strs.length; i++) {
-        let str = strs[i], zeros = 0, ones = 0
-        for (let j = 0; j < str.length; j++)
-            str.charAt(j) === "0" ? zeros++ : ones++
-        for (let j = m; j >= zeros; j--)
-            for (let k = n; k >= ones; k--)
-                dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1)
+    const dp = new Array(m + 1).fill(null).map(()=>new Array(n + 1));
+    let max = 0;
+    dp[0][0] = 0;
+    for(const str of strs) {
+        let zeros = 0, ones = 0;
+        for(let i = 0; i < str.length; i++) str.charAt(i) == '0' ? zeros++ : ones++;
+        for(let k = m; k >= zeros; k--) {
+            for(let l = n; l >= ones; l--) {
+                if(dp[k - zeros][l - ones] == null) continue
+                dp[k][l] = Math.max(dp[k][l] ?? 0, dp[k - zeros][l - ones] + 1);
+                max = Math.max(max, dp[k][l])
+            }
+        }
+
     }
-    return dp[m][n]
+    return max
 };

250b6d45122d9d177be846a099f86e1f8e1a694d
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-11-12 06:46:42 +0700

    Time: 83 ms (60.53%) | Memory: 60.7 MB (60.53%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.ts
+++ 474-ones-and-zeroes/ones-and-zeroes.ts
@@ -1,18 +1,14 @@
 function findMaxForm(strs: string[], m: number, n: number): number {
-    const dp = new Array(m + 1).fill(null).map(()=>new Array(n + 1));
-    let max = 0;
-    dp[0][0] = 0;
+    const dp = new Array(m + 1).fill(null).map(()=>new Array(n + 1).fill(0));
     for(const str of strs) {
         let zeros = 0, ones = 0;
         for(let i = 0; i < str.length; i++) str.charAt(i) == '0' ? zeros++ : ones++;
         for(let k = m; k >= zeros; k--) {
             for(let l = n; l >= ones; l--) {
-                if(dp[k - zeros][l - ones] == null) continue
                 dp[k][l] = Math.max(dp[k][l] ?? 0, dp[k - zeros][l - ones] + 1);
-                max = Math.max(max, dp[k][l])
             }
         }
 
     }
-    return max
+    return dp[m][n]
 };

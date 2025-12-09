becd7eb02d4f4cabe694a3fd04fc9decd2900fd8
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-12-09 15:35:59 +0700

    Time: 287 ms (50.00%) | Memory: 96.2 MB (66.67%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.ts
+++ 3885-count-special-triplets/count-special-triplets.ts
@@ -1,16 +1,16 @@
 const mod = 10 ** 9 + 7;
 function specialTriplets(nums: number[]): number {
-    const leftMap = {};
-    const rightMap = {};
+    const leftMap = new Map<number, number>();
+    const rightMap = new Map<number, number>();
     let triplets = 0;
-    for(let i = 0; i < nums.length; i++) {
-        rightMap[nums[i]] = (rightMap[nums[i]] ?? 0) + 1;
+    for(const n of nums) {
+        rightMap.set(n, (rightMap.get(n) ?? 0) + 1);
     }
     for(let i = 0; i < nums.length - 1; i++) {
-        rightMap[nums[i]]--;
+        rightMap.set(nums[i], (rightMap.get(nums[i]) ?? 0) - 1);
         const nextValue = nums[i] * 2;
-        triplets = (triplets + ((leftMap[nextValue] ?? 0) * (rightMap[nextValue] ?? 0)) % mod) % mod
-        leftMap[nums[i]] = (leftMap[nums[i]] ?? 0) + 1; 
+        triplets = (triplets + ((leftMap.get(nextValue) ?? 0) * (rightMap.get(nextValue) ?? 0)) % mod) % mod
+        leftMap.set(nums[i], (leftMap.get(nums[i]) ?? 0) + 1);
     }
     return triplets
 };

9a3292cd644737cc0c4d4d6e3426ba7ef844dd5b
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-12-10 07:22:02 +0700

    Time: 8 ms (0.00%) | Memory: 67.8 MB (0.00%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.ts
@@ -0,0 +1,10 @@
+const mod = 10 ** 9 + 7;
+function countPermutations(complexity: number[]): number {
+    // check wether invalid
+    if(complexity.length == 0 || complexity.slice(1).some((e) => e <= complexity[0])) return 0;
+    let result = 1;
+    for(let i = 1; i < complexity.length; i++) {
+        result = (result * i) % mod;
+    }
+    return result; 
+};

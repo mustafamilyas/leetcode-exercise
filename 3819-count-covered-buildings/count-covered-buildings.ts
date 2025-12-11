0d1028548dcf0af3b3b6025e967620d040e9dab7
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-12-11 07:42:20 +0700

    Time: 218 ms (20.00%) | Memory: 99.5 MB (80.00%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.ts
+++ 3819-count-covered-buildings/count-covered-buildings.ts
@@ -1,23 +1,21 @@
 function countCoveredBuildings(n: number, buildings: number[][]): number {
-    const rowMap = new Map<number, [number, number]>(); // [min, max];
-    const colMap = new Map<number, [number, number]>(); // [min, max];
+    const rowMin = Array.from({length: n + 1}, () => n + 1);
+    const rowMax = Array.from({length: n + 1}, () => 0);
+    const colMin = Array.from({length: n + 1}, () => n + 1);
+    const colMax = Array.from({length: n + 1}, () => 0);
     let covered = 0;
-    
-    for(const [row, col] of buildings) {
-        const storedRow = rowMap.get(row) ?? [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
-        storedRow[0] = Math.min(storedRow[0], col);  
-        storedRow[1] = Math.max(storedRow[1], col);
-        rowMap.set(row, storedRow)
 
-        const storedCol = colMap.get(col) ?? [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
-        storedCol[0] = Math.min(storedCol[0], row);  
-        storedCol[1] = Math.max(storedCol[1], row);  
-        colMap.set(col, storedCol)
+    for(const [row, col] of buildings) {
+        rowMin[row] = Math.min(rowMin[row], col);  
+        rowMax[row] = Math.max(rowMax[row], col);
+
+        colMin[col] = Math.min(colMin[col], row);  
+        colMax[col] = Math.max(colMax[col], row);  
     }
 
     for(const [row, col] of buildings) {
-        const [minCol, maxCol] = rowMap.get(row);
-        const [minRow, maxRow] = colMap.get(col);
+        const [minCol, maxCol] = [rowMin[row], rowMax[row]];
+        const [minRow, maxRow] = [colMin[col], colMax[col]];
         if(minCol < col && col < maxCol && minRow < row && row < maxRow) covered++;
     }
 

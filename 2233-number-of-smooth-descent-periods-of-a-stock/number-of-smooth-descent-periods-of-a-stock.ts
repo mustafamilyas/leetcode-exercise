fd8a84ea68f370bb0a2f3d90dc5f2022427633f6
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2025-12-15 07:18:23 +0700

    Time: 4 ms (66.67%) | Memory: 67.3 MB (100.00%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.ts
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.ts
@@ -2,7 +2,7 @@ function getDescentPeriods(prices: number[]): number {
     let continuos = 1;
     let descentPeriods = 0;
     for(let i = 0; i < prices.length; i++) {
-        if(prices[i - 1] - prices[i] == 1) continuos += 1; 
+        if(i > 0 && prices[i - 1] - prices[i] == 1) continuos += 1; 
         else continuos = 1;
         descentPeriods += continuos;
     }

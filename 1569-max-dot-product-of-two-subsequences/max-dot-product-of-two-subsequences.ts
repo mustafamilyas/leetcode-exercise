123fe85fcc8bdf72664e898848908a135111c8ab
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-01-08 10:12:43 +0700

    Time: 35 ms (0.00%) | Memory: 60 MB (100.00%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.ts
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.ts
@@ -1,25 +1,13 @@
 function maxDotProduct(nums1: number[], nums2: number[]): number {
-    const dp = Array.from({length: nums1.length}).map(
-        ()=>Array.from({length: nums2.length}).map(()=>0)
-    )
+    const dp = Array.from({ length: nums1.length }, () => new Array(nums2.length).fill(0))
     for(let i = 0; i < nums1.length; i++) {
         for(let k = 0; k < nums2.length; k++) {
-            const cur = nums1[i] * nums2[k]
-            if(k === 0 || i === 0) {
-                if(k > 0) {
-                    dp[i][k] = Math.max(cur, dp[i][k - 1])
-                } else if (i > 0) {
-                    dp[i][k] = Math.max(cur, dp[i - 1][k])
-                } else {
-                    dp[i][k] = cur;
-                }
-            } else {
-                const prevD = dp?.[i - 1]?.[k - 1] ?? 0;
-                const prevK = dp?.[i]?.[k - 1] ?? 0
-                const prevI = dp?.[i - 1]?.[k] ?? 0 
-                dp[i][k] = Math.max(prevD + cur, cur, prevD, prevK, prevI)
-            }
+            const top = dp?.[i - 1]?.[k] ?? Number.MIN_SAFE_INTEGER;
+            const left = dp?.[i]?.[k - 1] ?? Number.MIN_SAFE_INTEGER;
+            const prevSum = dp?.[i - 1]?.[k - 1] ?? 0;
+            const cur = nums1[i] * nums2[k];
+            dp[i][k] = Math.max(top, left, prevSum + cur, cur);
         }
     }
-    return dp[nums1.length - 1][nums2.length - 1]
+    return dp[nums1.length - 1][nums2.length - 1];
 };

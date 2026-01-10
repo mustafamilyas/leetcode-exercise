7a3d17193479c941889707269209bc7148cceb4e
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-01-10 20:52:51 +0700

    Time: 33 ms (42.86%) | Memory: 63.5 MB (57.14%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.ts
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.ts
@@ -2,21 +2,23 @@ function minimumDeleteSum(s1: string, s2: string): number {
     const n = s1.length;
     const m = s2.length;
     const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
-    let total = 0;
+    for(let i = 0; i < n; i++) {
+        dp[i + 1][0] = dp[i][0] + s1.charCodeAt(i);
+    }
+    for(let i = 0; i < m; i++) {
+        dp[0][i + 1] = dp[0][i] + s2.charCodeAt(i);
+    }
     for(let i = 0; i < n; i++) {
         const c1 = s1.charCodeAt(i);
-        total += c1;
         for(let k = 0; k < m; k++) {
             const c2 = s2.charCodeAt(k);
-            if(i == 0) total += c2;
-            if(c1 == c2) {
-                dp[i + 1][k + 1] = dp[i][k] + c1;
-            } else {
-                dp[i + 1][k + 1] = Math.max(dp[i][k + 1], dp[i + 1][k]) 
-            }
+            const top = (dp?.[i]?.[k + 1] ?? Number.MAX_SAFE_INTEGER) + c1;
+            const left = (dp?.[i+1]?.[k] ?? Number.MAX_SAFE_INTEGER) + c2;
+            const diag = (dp?.[i]?.[k] ?? 0) + (c1 == c2 ? 0 : c1 + c2);
+            dp[i + 1][k + 1] = Math.min(top, left, diag);
         }
     }
-    return total - 2 * dp[n][m];
+    return dp[n][m];
 };
 
 /**

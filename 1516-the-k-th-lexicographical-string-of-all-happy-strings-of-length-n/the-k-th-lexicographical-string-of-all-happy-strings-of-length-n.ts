934953b720bd8b0c9fe10197850c56724f4a3f92
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-03-14 08:20:26 +0700

    Time: 0 ms (100.00%) | Memory: 54.3 MB (100.00%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.ts
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.ts
@@ -1,27 +1,19 @@
 function getHappyString(n: number, k: number): string {
-    const char = ['a', 'b', 'c'];
-    let totalCombination = 3 * 2 ** (n - 1);
-    if(k > totalCombination) return '';
+    let total = 3;
+    for(let i = 1; i < n; i++) {
+        total *= 2;
+    } 
+    if(k > total) return "";
+    let str = "";
+    let div = total / 3;
     k--;
-    let prev = -1, res = '';
-    for(let i = 0; i < n; i++) {
-        let idx = -1;
-        if(i == 0) {
-            totalCombination /= 3;
-            idx = Math.floor(k / totalCombination);
-        } else {
-            totalCombination /= 2;
-            idx = resolveIdx(Math.floor(k / totalCombination), prev);
-        }
-        res += char[idx];
-        k %= totalCombination;
-        prev = idx;
+    while(str.length < n) {
+        let idx = Math.floor(k / div);
+        const prevCharCode = (str.charCodeAt(str.length - 1) || 99) - 97; 
+        if(prevCharCode == 0 || (prevCharCode == 1 && idx == 1)) idx++ 
+        str += String.fromCharCode(97 + idx);
+        k %= div;
+        div /= 2;
     }
-    return res;
+    return str;
 };
-
-function resolveIdx(cur: number, prev: number): number {
-    if(prev == 0) return cur + 1;
-    if(prev == 1) return cur === 0 ? cur : 2;
-    return cur;
-}

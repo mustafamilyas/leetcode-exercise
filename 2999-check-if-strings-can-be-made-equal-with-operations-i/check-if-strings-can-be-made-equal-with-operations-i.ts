a972494406915dc36fcfb197f84cfee5a352e0b4
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-03-29 12:19:29 +0700

    Time: 0 ms (100.00%) | Memory: 58.5 MB (44.44%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.ts
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.ts
@@ -1,16 +1,22 @@
+// function canBeEqual(s1: string, s2: string): boolean {
+//     const diff = 2;
+//     const counter = Array.from({ length: diff }, () => new Array(26).fill(0));
+//     for(let i = 0; i < s1.length; i++) {
+//         const y = s1.charCodeAt(i) - 97;
+//         const x = i % diff;
+//         counter[x][y]++;
+//     }
+//     for(let i = 0; i < s2.length; i++) {
+//         const y = s2.charCodeAt(i) - 97;
+//         const x = i % diff;
+//         if(counter[x][y] == 0) return false;
+//         counter[x][y]--;
+//     }
+//     return true;
+// };
+
 function canBeEqual(s1: string, s2: string): boolean {
-    const diff = 2;
-    const counter = Array.from({ length: diff }, () => new Array(26).fill(0));
-    for(let i = 0; i < s1.length; i++) {
-        const y = s1.charCodeAt(i) - 97;
-        const x = i % diff;
-        counter[x][y]++;
-    }
-    for(let i = 0; i < s2.length; i++) {
-        const y = s2.charCodeAt(i) - 97;
-        const x = i % diff;
-        if(counter[x][y] == 0) return false;
-        counter[x][y]--;
-    }
-    return true;
+    const evenIdx = (s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0])
+    const oddIdx = (s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1])
+    return !!evenIdx && !!oddIdx;
 };

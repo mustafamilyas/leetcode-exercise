1c8dc1772ee403680408cf15e308e27a13adb2bd
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-04-13 07:12:24 +0700

    Time: 1 ms (36.84%) | Memory: 56.2 MB (31.58%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.ts
@@ -0,0 +1,7 @@
+function getMinDistance(nums: number[], target: number, start: number): number {
+    let min = Number.MAX_SAFE_INTEGER;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] == target) min = Math.min(min, Math.abs(i - start));
+    }
+    return min;
+};

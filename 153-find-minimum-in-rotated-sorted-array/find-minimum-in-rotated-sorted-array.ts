aa2ce353da33ef27d156c6f34df8cfbdb4df9dbe
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-05-15 07:14:53 +0700

    Time: 0 ms (100.00%) | Memory: 56.3 MB (8.29%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,15 @@
+function findMin(nums: number[]): number {
+    const n = nums.length;
+    if(nums.length == 1 || nums[0] < nums[n - 1]) return nums[0];
+    let l = 0, r = n - 1;
+    while(l < r) {
+        const mid = Math.floor((l + r) / 2);
+        if(nums[mid] > nums[mid + 1]) return nums[mid + 1];
+        if(nums[mid] > nums[0]) {
+            l = mid
+        } else {
+            r = mid
+        }
+    }
+    return l
+};

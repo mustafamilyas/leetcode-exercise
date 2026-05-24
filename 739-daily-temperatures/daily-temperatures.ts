bf37cff19f136646ded66527eade64b8a1187b21
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-05-25 06:15:21 +0700

    Time: 23 ms (41.80%) | Memory: 80.9 MB (26.56%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.ts
+++ 739-daily-temperatures/daily-temperatures.ts
@@ -1,14 +1,11 @@
 function dailyTemperatures(temperatures: number[]): number[] {
-    const result = new Array(temperatures.length).fill(0)
-    const stack = [temperatures.length - 1];
-    for(let i = temperatures.length - 1; i >= 0; i--) {
-        while(stack.length && temperatures[stack[stack.length - 1]] <= temperatures[i]) {
-            stack.pop()
-        }
-        if(stack.length)  {
-            result[i] = stack[stack.length - 1] - i
-        }
-        stack.push(i)
+    const stack = [];
+    const n = temperatures.length;
+    const answer = new Array(n).fill(0);
+    for(let i = n - 1; i >= 0; i--) {
+        while(stack.length && temperatures[stack[stack.length - 1]] <= temperatures[i]) stack.pop()
+        answer[i] = (stack?.[stack.length - 1] ?? i) - i;
+        stack.push(i);
     }
-    return result;
+    return answer;
 };

106706662f4814f95390a3bb04c5149aafe4104f
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-06-13 15:10:25 +0700

    Time: 12 ms (5.56%) | Memory: 62.1 MB (6.67%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.ts
+++ 4216-weighted-word-mapping/weighted-word-mapping.ts
@@ -1,5 +1,6 @@
 function mapWordWeights(words: string[], weights: number[]): string {
     let res = '';
+    const weightMap: Record<string, number> = {};
     for(let i = 0; i < words.length; i++) {
         const weight = getWeight(words[i]);
         res += String.fromCharCode(122 - weight);
@@ -7,10 +8,12 @@ function mapWordWeights(words: string[], weights: number[]): string {
     return res;
 
     function getWeight(word: string): number {
+        if(typeof weightMap[word] == 'number') return weightMap[word]; 
         let sum = 0;
         for(let i = 0; i < word.length; i++) {
             sum += weights[word.charCodeAt(i) - 97];
         }
-        return sum % 26;
+        weightMap[word] = sum % 26;
+        return weightMap[word];
     }
 };

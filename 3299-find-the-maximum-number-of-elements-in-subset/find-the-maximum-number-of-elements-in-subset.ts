4d602b5e8926a03e396563a0396d5294eedb4dee
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-06-27 09:44:19 +0700

    Time: 45 ms (40.00%) | Memory: 71.6 MB (100.00%) - LeetSync

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.ts
@@ -0,0 +1,26 @@
+function maximumLength(nums: number[]): number {
+    const map = new Map();
+    let max = 1;
+    for(let i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) ?? 0) + 1)
+    }
+    for(const [key, value] of map) {
+        if(key == 1) {
+            max = Math.max(max, value % 2 ? value : value - 1);
+            continue;
+        }
+        let cur = key;
+        let curCount = 0;
+        while(map.get(cur) >= 2) {
+            cur *= cur;
+            curCount += 2;
+        }
+        if(map.get(cur) >= 1) {
+            curCount += 1;
+        } else if(curCount > 0) {
+            curCount -= 1;
+        }
+        max = Math.max(max, curCount)
+    }
+    return max;
+};

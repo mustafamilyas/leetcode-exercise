e46f028184e02153cd698dc2231f4b86c3674934
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-06-28 09:59:51 +0700

    Time: 13 ms (0.00%) | Memory: 66.3 MB (0.00%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.ts
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.ts
@@ -1,11 +1,10 @@
 function maximumElementAfterDecrementingAndRearranging(arr: number[]): number {
-    arr.sort((a,b)=>a-b);
-    for(let i = 0; i < arr.length; i++) {
-        const prev = arr[i - 1] ?? 0;
-        const cur = arr[i]
-        if((cur - prev) > 1) {
-            arr[i] = prev + 1
+    arr.sort((a, b)=> a - b);
+    arr[0] = 1;
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] - arr[i - 1] > 1) {
+            arr[i] = arr[i - 1] + 1;
         }
     }
-    return arr[arr.length - 1];
+    return arr[arr.length - 1]
 };

aacad53e12db5b60988b0791aeb89b56f1587afa
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-07-13 07:28:07 +0700

    Time: 0 ms (100.00%) | Memory: 55.2 MB (50.00%) - LeetSync

--- 1212-sequential-digits/sequential-digits.ts
+++ 1212-sequential-digits/sequential-digits.ts
@@ -1,14 +1,14 @@
+const MAX_DIGIT = 9;
 function sequentialDigits(low: number, high: number): number[] {
-    const lowest = Math.floor(Math.log10(low));
-    const highest = Math.floor(Math.log10(high));
-    const result = []
-    for(let i = lowest; i <= highest; i++) {
-        let content = Math.floor(123456789 / (10 ** (9 - i - 1)));
-        let adder = Math.floor(111111111 / (10 ** (9 - i - 1)));
-        for(let k = 1; k <= 9 - i; k++) {
-            if(content >= low && content <= high) result.push(content)
-            content += adder
+    const res = [];
+    const low_digit = Math.trunc(Math.log10(low)), high_digit = Math.trunc(Math.log10(high));
+    for(let digit = low_digit; digit <= high_digit; digit++) {
+        let cur = Math.trunc(123456789 / 10 ** (MAX_DIGIT - digit - 1));
+        const adder = Math.trunc(111111111 / 10 ** (MAX_DIGIT - digit - 1));
+        for(let i = 0; i < MAX_DIGIT - digit; i++) {
+            if(cur >= low && cur <= high) res.push(cur)
+            cur += adder;
         }
     }
-    return result
+    return res; 
 };

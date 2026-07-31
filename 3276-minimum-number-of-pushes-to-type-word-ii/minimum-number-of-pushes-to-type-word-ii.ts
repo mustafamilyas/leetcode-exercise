d18df9e1046c8805c0a8693e7a2794bdf8d41351
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-07-31 07:16:51 +0700

    Time: 12 ms (100.00%) | Memory: 62.8 MB (85.71%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.ts
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.ts
@@ -1,12 +1,14 @@
 function minimumPushes(word: string): number {
-    const chars = new Array(26).fill(0);
-    for(let i = 0; i < word.length; i++) {
-        chars[word.charCodeAt(i) - 97]++
-    }
-    chars.sort((a, b)=>b-a);
+    const count = new Array(26).fill(0);
     let push = 0;
-    for(let i = 0; i < chars.length && chars[i] > 0; i++) {
-        push += Math.ceil((i + 1) / 8) * chars[i]
+    for(let i = 0; i < word.length; i++) {
+        const idx = word.charCodeAt(i) - 97;
+        count[idx]++;
+    }
+    count.sort((a, b) => b - a);
+    for(let i = 0; i < count.length; i++) {
+        const mul = Math.ceil((i + 1) / 8);
+        push += mul * count[i]
     }
     return push;
 };

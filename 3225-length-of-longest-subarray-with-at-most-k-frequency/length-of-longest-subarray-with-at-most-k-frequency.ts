cd3e98d1ddd275bfdca5c13d2d460dc3d77482a8
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-08-12 07:16:56 +0700

    Time: 55 ms (87.50%) | Memory: 75.8 MB (56.25%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.ts
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.ts
@@ -1,12 +1,17 @@
 function maxSubarrayLength(nums: number[], k: number): number {
-    let l = 0, counter = {}, max = 0;
-    for(let r = 0; r < nums.length; r++) {
-        const curNum = nums[r];
-        counter[curNum] = (counter[curNum] ?? 0) + 1;
-        if(counter[curNum] > k) {
-            while(counter[curNum] > k && l < r) --counter[nums[l++]]
+    const map = new Map();
+    let left = 0, max = 0;
+    for(let i = 0; i < nums.length; i++) {
+        const cur = nums[i];
+        const curCount = (map.get(cur) ?? 0) + 1;
+        map.set(cur, curCount);
+        if(curCount > k) {
+            while(left < i && map.get(cur) > k) {
+                const leftItem = nums[left++];
+                map.set(leftItem, map.get(leftItem) - 1)
+            }
         }
-        max = Math.max(max, r - l + 1)
+        max = Math.max(max, i - left + 1)
     }
     return max;
 };

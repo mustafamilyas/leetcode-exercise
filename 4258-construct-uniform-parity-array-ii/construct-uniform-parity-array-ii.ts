799a49c6804e7a9d22a3dcfde4c975b483c020dd
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-09-03 19:45:08 +0700

    Time: 6 ms (42.86%) | Memory: 73.9 MB (85.71%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.ts
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.ts
@@ -1,7 +1,7 @@
 function uniformArray(nums1: number[]): boolean {
     let minEven = Number.MAX_SAFE_INTEGER, minOdd = Number.MAX_SAFE_INTEGER;
     for(let i = 0; i < nums1.length; i++) {
-        if(nums1[i] % 2) minOdd = Math.min(minOdd, nums1[i])
+        if(nums1[i] & 1) minOdd = Math.min(minOdd, nums1[i])
         else minEven = Math.min(minEven, nums1[i])
     }
     if(minEven == Number.MAX_SAFE_INTEGER || minOdd == Number.MAX_SAFE_INTEGER) return true;

462a9f346872112302a8daa85e491645b46d9683
Muhamad Ilyas Mustafa <36124005+mustafamilyas@users.noreply.github.com>
2026-09-08 17:19:05 +0700

    Time: 3 ms (40.00%) | Memory: 58.4 MB (36.36%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.ts
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.ts
@@ -1,18 +1,15 @@
 function minElement(nums: number[]): number {
-    const memo = new Map<number, number>();
     let min = Infinity;
     for(const v of nums) {
         min = Math.min(digitSum(v), min);
     }
     return min;
     function digitSum(n: number): number {
-        if(memo.has(n)) return memo.get(n);
         let sum = 0, cur = n;
         while(cur > 0) {
             sum += (cur % 10);
             cur = Math.trunc(cur / 10);
         }
-        memo.set(n, sum);
         return sum;
     }
 };
